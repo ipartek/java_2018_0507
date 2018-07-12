4d7e943ee98dcfe2746756491dcf52375d05ef99
Peybool <pablomartin9696@gmail.com>
2018-07-12 13:47:10 +0200

    Hola Caracola");

--- Ejemplo1/src/com/ipartek/formacion/ejemplo1/Prueba1.java
+++ Ejemplo1/src/com/ipartek/formacion/ejemplo1/Prueba1.java
@@ -7,7 +7,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Hola Mikel Gil");
 		System.out.println("Hola Carlos");
-		System.out.println("Hola Caracola");
+		
 	}
 	
 	public static void mainArrayBidimensional(String[] args) {
