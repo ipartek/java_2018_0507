719e26703cb6cb4bb55566d5d13006932e8f1286
Koldo Iglesias <info@koldoiglesias.com>
2018-07-16 14:26:44 +0200

    Ejercicios básicos secuenciales

--- Ejemplo1/src/com/ipartek/formacion/ejercicios/basicos/Main.java
+++ Ejemplo1/src/com/ipartek/formacion/ejercicios/basicos/Main.java
@@ -1,11 +1,12 @@
 package com.ipartek.formacion.ejercicios.basicos;
 
 import com.ipartek.formacion.ejercicios.basicos.Basicos;
+import com.ipartek.formacion.ejercicios.basicos.BasicosSecuencial;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Basicos.ejerBasico1(477, 144.75, 'R');
+		/*Basicos.ejerBasico1(477, 144.75, 'R');
 		Basicos.ejerBasico2(24, 78, 94.55, 74.25);
 		Basicos.ejerBasico3(55);
 		Basicos.ejerBasico4(1, 2, 3, 4);
@@ -15,6 +16,8 @@ public class Main {
 		Basicos.ejerBasico7(8);
 		Basicos.ejerBasico7(255);
 		Basicos.ejerBasico7(-850);
+		BasicosSecuencial.BasicosSecuencial1();*/
+		BasicosSecuencial.BasicosSecuencial5();
 	}
 
 }
