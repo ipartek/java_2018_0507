eec30a82a4dd38b36aafca6ba1a4fe63c64e0d02
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-07-18 14:17:21 +0200

    VideoClub casi acabado

--- Ejemplo1/src/com/ipartek/formacion/video/pojo/VideoYoutube.java
+++ Ejemplo1/src/com/ipartek/formacion/video/pojo/VideoYoutube.java
@@ -2,44 +2,6 @@ package com.ipartek.formacion.video.pojo;
 
 public class VideoYoutube {
 
-//	private long id;
-//	private String codigo;
-//	private String titulo;
-//	
-//	
-//	
-//	
-//	public VideoYoutube() { //Utilizando el constructor de abajo vale con esto
-//		this(-1,"","");
-//
-//	}
-//	
-//	public VideoYoutube(long id, String codigo, String titulo) {
-//		super();
-//		this.id = id;
-//		this.codigo = codigo;
-//		this.titulo = titulo;
-//	}
-//	public long getId() {
-//		return id;
-//	}
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-//	public String getCodigo() {
-//		return codigo;
-//	}
-//	public void setCodigo(String codigo) {
-//		this.codigo = codigo;
-//	}
-//	public String getTitulo() {
-//		return titulo;
-//	}
-//	public void setTitulo(String titulo) {
-//		this.titulo = titulo;
-//	}
-//	
-	
 	private long id;
 	private String codigo;
 	private String titulo;
@@ -50,7 +12,7 @@ public class VideoYoutube {
 		this.titulo = titulo;
 	}
 
-	public VideoYoutube() {
+	public VideoYoutube() { //Utilizando el constructor de arriba vale con esto
 		this(-1, "", "");
 	}
 
--- Ejemplo1/src/com/ipartek/formacion/video/presentacionconsola/VideoYoutubeMain.java
+++ Ejemplo1/src/com/ipartek/formacion/video/presentacionconsola/VideoYoutubeMain.java
@@ -1,5 +1,7 @@
 package com.ipartek.formacion.video.presentacionconsola;
 
+import java.util.Scanner;
+
 import com.ipartek.formacion.video.accesodatos.CrudAble;
 import com.ipartek.formacion.video.accesodatos.VideoYoutubeArrayDAO;
 import com.ipartek.formacion.video.pojo.VideoYoutube;
@@ -7,21 +9,50 @@ import com.ipartek.formacion.video.pojo.VideoYoutube;
 public class VideoYoutubeMain {
 
 	private static final int LISTADO = 1;
+	private static final int BUSQUEDA = 2;
+	private static final int INSERTAR = 3;
+	private static final int ACTUALIZAR = 4;
+	private static final int BORRAR = 5;
+	private static final int SALIR = 0;
+	
 
 	public static void main(String[] args) {
 		cargarVideos();
 		
 		mostrarMenu();
 		
-		//TODO: Hacer el menú que se repita cada vez e implementar la lectura de teclado
-		int opcion = LISTADO;
+		elegirOpcion();
+			
+		
+	}
+
+
+	private static void elegirOpcion() {
+		Scanner sc = new Scanner(System.in);
+		int opcion =  sc.nextInt();
 		
 		switch(opcion) {
 		case LISTADO:
 			listadoVideos();
 			break;
+		case BUSQUEDA:
+			BusquedaVideo();
+			break;
+		case INSERTAR:
+			InsertarVideo();
+			break;
+		case ACTUALIZAR:
+			ActualizarVideo();
+			break;
+		case BORRAR:
+			BorrarVideo();
+			break;
+		case SALIR:
+			p("Venga, hasta luego");
+			break;
 		}
 	}
+
 
 	private static void mostrarMenu() {
 		p("------------");
@@ -29,9 +60,14 @@ public class VideoYoutubeMain {
 		p("------------");
 		p("");
 		p("1. Listado de videos");
-		p("...");
+		p("2. Búsqueda de video");
+		p("3. Insertar video");
+		p("4. Actualizar datos video");
+		p("5. Borrar Video");
 		p("0. Salir");
 		p("Elige una opción");
+		
+	
 	}
 	
 	private static void listadoVideos() {
@@ -42,6 +78,8 @@ public class VideoYoutubeMain {
 		for(VideoYoutube video: dao.getAll()) {
 			mostrarVideo(video);
 		}
+		mostrarMenu();
+		elegirOpcion();
 	}
 
 	private static void mostrarCabecera() {
@@ -64,5 +102,80 @@ public class VideoYoutubeMain {
 	public static void p(String s) {
 		System.out.println(s);
 	}
+	
+	private static void InsertarVideo() {
+		CrudAble<VideoYoutube> dao = VideoYoutubeArrayDAO.getInstance();
+		
+		Scanner sc = new Scanner(System.in);
+
+		
+		System.out.println("Inserta el ID");
+		String id = sc.nextLine();
+		int id2=Integer.parseInt(id);
+		System.out.println("Inserta el Codigo");
+		String cod = sc.nextLine();
+		System.out.println("Inserta el Titulo");
+		String tit = sc.nextLine();
+		
+		dao.insert(new VideoYoutube (id2,cod,tit));
+		
+		mostrarMenu();
+		elegirOpcion();
+	}
+	
+	private static void BusquedaVideo() {
+		
+		CrudAble<VideoYoutube> dao = VideoYoutubeArrayDAO.getInstance();
+		
+		Scanner sc = new Scanner(System.in);
+		p("Inserta el ID del video que quieres buscar");
+		String id = sc.nextLine();
+		int id2=Integer.parseInt(id);
+		System.out.println(dao.getById(id2));
+		
+		mostrarMenu();
+		elegirOpcion();
+	}
+
+
+	private static void BorrarVideo() {
+		
+		CrudAble<VideoYoutube> dao = VideoYoutubeArrayDAO.getInstance();
+		
+		Scanner sc = new Scanner(System.in);
+		p("Que video quieres borrar?");
+		String id = sc.nextLine();
+		int id2=Integer.parseInt(id);
+		dao.delete(id2);
+		
+		listadoVideos();
+		
+	}
+	
+
+	private static void ActualizarVideo() {
+		
+		CrudAble<VideoYoutube> dao = VideoYoutubeArrayDAO.getInstance();
+		
+		Scanner sc = new Scanner(System.in);
+		p("Que video quieres actualizar? Inserta su ID");
+		String id = sc.nextLine();
+		int id2=Integer.parseInt(id);
+
+		dao.getById(id2);
+		
+		System.out.println("Inserta el nuevo ID");
+		String idc = sc.nextLine();
+		int idc2=Integer.parseInt(id);
+		System.out.println("Inserta el nuevo Codigo");
+		String cod = sc.nextLine();
+		System.out.println("Inserta el nuevo Titulo");
+		String tit = sc.nextLine();
+		
+		
+		dao.update(new VideoYoutube(idc2,cod,tit));
+		
+	}
+	
 
 }
