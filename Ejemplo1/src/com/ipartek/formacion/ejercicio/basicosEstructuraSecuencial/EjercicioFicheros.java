65558f769bcd5b1b0b05c28a1b1c1bc507bf4c69
Carlos Ekaitz Ugalde Martinez <carlosekaitzugalde@gmail.com>
2018-07-18 12:35:04 +0200

    previoEjercicio

---
+++ Ejemplo1/src/com/ipartek/formacion/ejercicio/basicosEstructuraSecuencial/EjercicioFicheros.java
@@ -0,0 +1,12 @@
+//Leer lineas de texto por teclado y escribirlas en un fichero de texto.
+package com.ipartek.formacion.ejercicio.basicosEstructuraSecuencial;
+
+import java.io.File;
+import java.nio.file.FileAlreadyExistsException;
+
+public class EjercicioFicheros {
+
+	//File archivo = new File(FileAlreadyExistsException);
+	//FileAlreadyExistsException File new file sobreescribe.//
+	//comprobar si sse le llama o es una excepcion
+}
---
+++ Ejemplo1/src/com/ipartek/formacion/video/presentacionconsola/VideoYoutubeMain.java
@@ -0,0 +1,52 @@
+package com.ipartek.formacion.video.presentacionconsola;
+
+import com.ipartek.formacion.video.accesoDatos.CrudAble;
+import com.ipartek.formacion.video.accesoDatos.VideoYoutubeArrayDAO;
+import com.ipartek.formacion.video.pojo.VideoYoutube;
+
+public class VideoYoutubeMain {
+	
+	public static void main(String[] args) {
+		cargarVideos();
+		
+		p("------------");
+		p("VideoYoutube");
+		p("------------");
+		p("");
+		p("1. Listado de videos");
+		p("...");
+		p("Elige una opcion");
+		
+		//TODO Hacer el menu que se repita cada vez e implementar lectura de teclado
+
+		int opcion = 1;
+		
+		switch(opcion) {
+		case 1:
+		listadoVideos();
+		break;
+		}
+	}
+	private static void listadoVideos() {
+		CrudAble<VideoYoutube> dao =VideoYoutubeArrayDAO.getInstance();
+		
+		p("id\tCodigo\tTitulo");
+		
+		for (VideoYoutube video: dao.getAll()) {
+			mostrarVideo(video);
+		}
+	}
+	private static void mostrarVideo(VideoYoutube video) {
+		p(video)
+		
+	}
+	private static void cargarVideos() {
+		CrudAble<VideoYoutube> dao =VideoYoutubeArrayDAO.getInstance();
+		for(int i =1; i<=10;i++) {
+			dao.insert(new VideoYoutube(1, "asdfg", "mi video"));
+		}
+	}
+	public static void p(String s) {
+		System.out.println(s);
+	}
+}
