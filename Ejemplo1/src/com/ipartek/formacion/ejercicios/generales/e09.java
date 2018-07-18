169dd228414726b365d580cba673938eed46bece
Curso <Curso@Ipartek-TOSH05.ipartekaula.com>
2018-07-18 12:43:21 +0200

    a

--- Ejemplo1/src/com/ipartek/formacion/ejercicios/generales/e09.java
+++ Ejemplo1/src/com/ipartek/formacion/ejercicios/generales/e09.java
@@ -1,12 +1,37 @@
 package com.ipartek.formacion.ejercicios.generales;
+
+import java.util.Scanner;
+
 /**
- * Programa que pasa un número
- * de decimal a binario
+ * Programa que pasa un número de decimal a binario
  */
 public class e09 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int numero;
+		int potencia;
+		int digito;
+		int binario;
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.print("Introduce un numero entero: ");
+			numero = sc.nextInt();
+		} while (numero < 0);
+
+		potencia = 0;
+		binario = 0;
+		while (numero != 0) {
+			digito = numero % 2;
+			binario = (int) (binario + digito * Math.pow(10, potencia));
+			potencia++;
+			numero = numero / 2;
+		}
+		System.out.printf("Binario: " + binario);
+
+		sc.close();
 
 	}
 
--- Ejemplo1/src/com/ipartek/formacion/ejercicios/generales/e10.java
+++ Ejemplo1/src/com/ipartek/formacion/ejercicios/generales/e10.java
@@ -1,10 +1,46 @@
 package com.ipartek.formacion.ejercicios.generales;
 
+import java.util.Scanner;
+
 public class e10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		long numero, aux, digito, decimal;
+		int exponente;
+		boolean esBinario;
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.print("Introduce un numero binario: ");
+			numero = sc.nextLong();
+			esBinario = true;
+			aux = numero;
+			while (aux != 0) {
+				digito = aux % 10; // última cifra del números
+				if (digito != 0 && digito != 1) { // si no es 0 ó 1
+					esBinario = false; // no es un número binario
+				}
+				aux = aux / 10; // quitamos la última cifra para repetir el proceso
+			}
+		} while (!esBinario); // se vuelve a pedir si no es binario
+
+		// proceso para pasar de binario a decimal
+		exponente = 0;
+		decimal = 0; // será el equivalente en base decimal
+		while (numero != 0) {
+			// se toma la última cifra
+			digito = numero % 10;
+			// se multiplica por la potencia de 2 correspondiente y se suma al número
+			decimal = decimal + digito * (int) Math.pow(2, exponente);
+			// se aumenta el exponente
+			exponente++;
+			// se quita la última cifra para repetir el proceso
+			numero = numero / 10;
+		}
+		System.out.println("Decimal: " + decimal);
+		sc.close();
 	}
 
 }
