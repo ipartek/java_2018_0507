4fc871ba0631c90308c2b78dd831ab0f9a449655
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-07-19 09:59:28 +0200

    cambios

---
+++ Ejemplo1/src/com/ipartek/formacion/GestionLibros/Cliente.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.GestionLibros;
+
+import java.util.List;
+
+public class Cliente {
+	private long id;
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private List<Libro> LibrosPrestados;
+}
---
+++ Ejemplo1/src/com/ipartek/formacion/GestionLibros/Libro.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.GestionLibros;
+
+public class Libro {
+	private long id;
+	private String isbn;
+	private String titulo;
+	private String autor;
+	private String editorial;
+	private Cliente cliente;
+}
