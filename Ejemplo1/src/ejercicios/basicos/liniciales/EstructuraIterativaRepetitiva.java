5ecd9fc7fbfc2e2ffe6b594fd3e8d4d626e0a8bd
Aitor84 <aitor_casado@hotmail.com>
2018-07-19 08:49:28 +0200

    Ejercicios Básicos con Estructura Iterativa o Repetitiva 4-6

--- Ejemplo1/src/ejercicios/basicos/liniciales/EstructuraIterativaRepetitiva.java
+++ Ejemplo1/src/ejercicios/basicos/liniciales/EstructuraIterativaRepetitiva.java
@@ -3,6 +3,32 @@ package ejercicios.basicos.liniciales;
 public class EstructuraIterativaRepetitiva {
 	
 	public static void main(String[] args) {
+		int n = 100;
+		for( n=100 ; n>=0; n--) {
+			System.out.println(n);
+		}
+	}
+
+	
+	public static void main05(String[] args) {
+		int n = 100;
+		do {
+			System.out.println(n);
+			n--;
+		}
+		while (n>=1);
+	}
+
+	public static void main04(String[] args) {
+		int n = 100;
+		while (n>=0) {
+			System.out.println(n);
+			n--;
+		}
+	}
+
+	
+	public static void main03(String[] args) {
 		int numero;
 		for (numero=0 ; numero<=100 ; numero++) {
 			System.out.println(numero);
