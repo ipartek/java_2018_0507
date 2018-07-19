922f0f5e5a04ec302d56b7ccb4e763cd7c251173
Javier <jglete@gmail.com>
2018-07-19 14:04:16 +0200

    Validaciones de numéricos

--- Ejemplo1/src/com/ipartek/formacion/video/presentacionconsola/Utils.java
+++ Ejemplo1/src/com/ipartek/formacion/video/presentacionconsola/Utils.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.video.presentacionconsola;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utils {
@@ -8,16 +9,50 @@ public class Utils {
 	public static String leerLinea() {
 		return s.nextLine();
 	}
-	
+
 	public static long leerLong() {
-		long l = s.nextLong();
+		long l = 0;
+		boolean hayQueRepetir;
+		
+		do {
+			try {
+				hayQueRepetir = false;
+				l = s.nextLong();
+			} catch (InputMismatchException e) {
+				System.out.println("Por favor, introduce un número entero sin comas ni puntos");
+				s.nextLine();
+				hayQueRepetir = true;
+			} catch (Exception e) {
+				System.out.println("Error no esperado");
+				throw e;
+			}
+		} while (hayQueRepetir);
+		
 		s.nextLine();
+		
 		return l;
 	}
-	
+
 	public static int leerInt() {
-		int i = s.nextInt();
+		int i = 0;
+		boolean hayQueRepetir;
+		
+		do {
+			try {
+				hayQueRepetir = false;
+				i = s.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Por favor, introduce un número entero sin comas ni puntos");
+				s.nextLine();
+				hayQueRepetir = true;
+			} catch (Exception e) {
+				System.out.println("Error no esperado");
+				throw e;
+			}
+		} while (hayQueRepetir);
+		
 		s.nextLine();
+		
 		return i;
 	}
 }
