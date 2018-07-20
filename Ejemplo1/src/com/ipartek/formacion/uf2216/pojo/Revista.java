f74c89ce5b1e8e4c894acebb80c5db42b3eb6099
Carlos Ekaitz Ugalde Martinez <carlosekaitzugalde@gmail.com>
2018-07-20 09:52:25 +0200

    cambiadas variables en mayuscaulas de clase revista

--- Ejemplo1/src/com/ipartek/formacion/uf2216/pojo/Revista.java
+++ Ejemplo1/src/com/ipartek/formacion/uf2216/pojo/Revista.java
@@ -2,75 +2,83 @@ package com.ipartek.formacion.uf2216.pojo;
 
 public class Revista {
 	private long id;
-	private String Titulo;
-	private String ISBN;
+	private String titulo;
+	private String isbn;
 	private long numPaginas;
 	private boolean formato;
 	//digital = true	papel= false
 	
 	public Revista() {
-		super();
+	this(-1, "", "", 0, false);
 	}
-	
-	/*public Libro() {
-		this(-1, "", "", "", "", false);
-	}*/
-	
-	
-	public Revista(long id, String titulo, String iSBN, long numPaginas, boolean formato) {
+
+	public Revista(long id, String titulo, String isbn, long numPaginas, boolean formato) {
 		super();
 		this.id = id;
-		Titulo = titulo;
-		ISBN = iSBN;
+		this.titulo = titulo;
+		this.isbn = isbn;
 		this.numPaginas = numPaginas;
 		this.formato = formato;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
-		return Titulo;
+		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
-		Titulo = titulo;
+		this.titulo = titulo;
 	}
-	public String getISBN() {
-		return ISBN;
+
+	public String getIsbn() {
+		return isbn;
 	}
-	public void setISBN(String iSBN) {
-		ISBN = iSBN;
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
 	}
+
 	public long getNumPaginas() {
 		return numPaginas;
 	}
+
 	public void setNumPaginas(long numPaginas) {
 		this.numPaginas = numPaginas;
 	}
+
 	public boolean isFormato() {
 		return formato;
 	}
+
 	public void setFormato(boolean formato) {
 		this.formato = formato;
 	}
+
 	@Override
 	public String toString() {
-		return "Revistas [id=" + id + ", Titulo=" + Titulo + ", ISBN=" + ISBN + ", numPaginas=" + numPaginas
+		return "Revista [id=" + id + ", titulo=" + titulo + ", isbn=" + isbn + ", numPaginas=" + numPaginas
 				+ ", formato=" + formato + "]";
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((ISBN == null) ? 0 : ISBN.hashCode());
-		result = prime * result + ((Titulo == null) ? 0 : Titulo.hashCode());
 		result = prime * result + (formato ? 1231 : 1237);
 		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
 		result = prime * result + (int) (numPaginas ^ (numPaginas >>> 32));
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -80,21 +88,21 @@ public class Revista {
 		if (getClass() != obj.getClass())
 			return false;
 		Revista other = (Revista) obj;
-		if (ISBN == null) {
-			if (other.ISBN != null)
-				return false;
-		} else if (!ISBN.equals(other.ISBN))
-			return false;
-		if (Titulo == null) {
-			if (other.Titulo != null)
-				return false;
-		} else if (!Titulo.equals(other.Titulo))
-			return false;
 		if (formato != other.formato)
 			return false;
 		if (id != other.id)
 			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
 		if (numPaginas != other.numPaginas)
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
 			return false;
 		return true;
 	}
--- Ejemplo1/src/com/ipartek/formacion/uf2216/presentacion/RevistasMain.java
+++ Ejemplo1/src/com/ipartek/formacion/uf2216/presentacion/RevistasMain.java
@@ -1,8 +1,11 @@
 package com.ipartek.formacion.uf2216.presentacion;
 
+import java.util.Scanner;
+
 import com.ipartek.formacion.uf2216.accesodatos.CrudAble;
 import com.ipartek.formacion.uf2216.accesodatos.RevistasArrayDAO;
 import com.ipartek.formacion.uf2216.pojo.Revista;
+import com.ipartek.prestamolibros.Libro;
 
 
 
@@ -10,11 +13,118 @@ import com.ipartek.formacion.uf2216.pojo.Revista;
 
 public class RevistasMain {
 	
-	static CrudAble<Revista> dao = RevistasArrayDAO.getInstance();
-
+	static CrudAble<Revista> dao = 
+			RevistasArrayDAO.getInstance();
+	public static long id;
+	public static String titulo;
+	public static String isbn;
+	public static long numPaginas;
+	public static boolean formato;
+	public static int opcionMenu;
+	public static boolean salir = true;
+	static Scanner  sc = new Scanner(System.in);
+	static Scanner  sc2 = new Scanner(System.in);
+	static LeerTeclado lt = new LeerTeclado(sc);
+	static LeerTeclado lt2 = new LeerTeclado(sc2);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		cargarRevistas();//TODO Crear una lista de revistas
+		mostrarMenu();
 
 	}
 
+	private static void cargarRevistas() {
+		//cargar una lista
+		
+	}
+
+	private static void mostrarMenu() {
+		//Mostrar el menu
+		p("------------");
+		p("--Revistas--");
+		p("------------");
+		p("");
+		p("1. Listado de revistas");
+		p("...");
+		p("2. Añadir Revista");
+		p("...");
+		p("0. Salir");
+		p("...");
+		p("Elige una opción");
+		opcionMenu(opcionMenu);
+		
+	}
+
+	private static void opcionMenu(int opcionMenu2) {
+		// Elegir que opcion del menu desea
+		System.out.println();
+		do {
+			opcionMenu = lt.leerInt(opcionMenu);
+			salir = true;
+		switch(opcionMenu) {
+		case 1: 
+			listarRevistas();
+			volver();
+			break;
+		case 2:
+			AñadirRevistas();
+			volver();
+			break;
+		case 0:
+			p("Adios");
+			break;
+		default:
+			salir = false;
+			System.out.println("Eleccion no valida. \nVuelva a elegir una opcion");
+			break;
+		}
+		}while (salir==false);
+	}
+
+	private static void AñadirRevistas() {
+		long id = 0;
+		String format = null;
+		listarRevistas();
+		p("Introduzca un id que no este en la lista");
+		id = lt.leerLong(id);
+		p("Introduzca el ISBN");
+		isbn = lt2.leerString(isbn);
+		p("Introduzca el titulo");
+		titulo = lt2.leerString(titulo);
+		p("Introduzca el número de paginas");
+		numPaginas = lt2.leerLong(numPaginas);
+		p("Introduzca el formato");
+		format = lt2.leerString(format);
+	
+		dao.insert(new Revista(id, titulo,  isbn, numPaginas, formato));
+		
+	}
+
+	private static void listarRevistas() {
+		mostrarCabeceraRevistas();
+		
+		for(Revista revista: dao.getAll()) {
+			mostrarRevista(revista);
+		}
+		
+	}
+	private static void mostrarCabeceraRevistas() {
+		p("ID\tTITULO\tISBN\t\tNº PAGINAS\tFORMATO");
+		
+	}
+
+	private static void mostrarRevista(Revista revista) {
+		p(revista.getId() + "\t" + revista.getIsbn() + "\t" + revista.getTitulo() + "\t" + libro.getAutor() + 
+				"\t" + revista.getEditorial());
+		
+	}
+
+	private static void volver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void p(String s) {
+		System.out.println(s);
+	}
 }
