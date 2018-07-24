30ef0452f1dafdc26eaec762b4a764bb8d3fbe6a
Curso <Curso@PORT-Z5.ipartekaula.com>
2018-07-24 13:41:50 +0200

    maven servlet coger ruta nombre

--- Ejemplo1/javaeeanamamm/src/main/java/com/ipartek/formacion/javaeeanamamm/servlets/LoginServlet.java
+++ Ejemplo1/javaeeanamamm/src/main/java/com/ipartek/formacion/javaeeanamamm/servlets/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.javaeeanamamm.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +18,17 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("Hola a todos: " + new java.util.Date());
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String ruta= request.getContextPath();
+		pw.println(ruta);
+		
+		//para coger parametros de la ruta cuando añades por ejemplo ?nombre=ana
+		String nombre = request.getParameter("nombre");
+		pw.append("Hola " ).append(nombre);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
