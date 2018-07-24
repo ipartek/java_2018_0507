42e3cdf2da9303f787ec86c704df60f606ca33e3
Curso <Curso@PORT-Z5.ipartekaula.com>
2018-07-24 13:19:29 +0200

    Maven con servlet

---
+++ Ejemplo1/javaeeanamamm/src/main/java/com/ipartek/formacion/javaeeanamamm/servlets/LoginServlet.java
@@ -0,0 +1,31 @@
+package com.ipartek.formacion.javaeeanamamm.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
