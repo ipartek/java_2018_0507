b95ff0fc8a06d56cf58a293729452700679ff096
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-07-24 13:55:44 +0200

    Servlet

---
+++ javaee/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.ipartek.formacion.javaee.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("Hola a todos: " + new java.util.Date());
+		
+		String ruta = request.getContextPath();
+		pw.println(ruta);
+		
+		String nombre = request.getParameter("nombre");
+		pw.append("Hola ").append(nombre);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
