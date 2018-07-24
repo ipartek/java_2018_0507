e3e2246533c39ec4376093f066fe1e0ace2a2db6
Curso <Curso@LAPTOP-95OBND8K.ipartekaula.com>
2018-07-24 13:41:32 +0200

    servlet

---
+++ javaeeborja/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.ipartek.formacion.javaee.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter pw=response.getWriter();
+		pw.println("hola a todos\n"+new java.util.Date());
+		
+		String ruta=request.getContextPath();
+		pw.println(ruta);
+		String nombre=request.getParameter("nombre");
+		pw.append("Hola ").append(nombre);
+	
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
