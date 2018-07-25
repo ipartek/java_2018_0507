da57f7970a3b6e87f403c507630f37eb9f56e8a3
Mikel Gil <m.gil05@gmail.com>
2018-07-25 09:43:45 +0200

    asdf

--- javaeeMikelGIl/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
+++ javaeeMikelGIl/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
@@ -26,6 +26,10 @@ public class LoginServlet extends HttpServlet {
 		pw.println(ruta);
 
 		String nombre = request.getParameter("nombre");
+		if (nombre == null) {
+			nombre ="DESCONOCIDO";
+		}
+		
 		pw.append("Hola ").append(nombre);
 
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
