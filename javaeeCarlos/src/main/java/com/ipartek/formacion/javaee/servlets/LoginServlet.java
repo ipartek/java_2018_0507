b7949fad05ccbf8acf8b3f9537ad5866e01fcd72
Carlos Ekaitz Ugalde Martinez <carlosekaitzugalde@gmail.com>
2018-07-25 10:45:25 +0200

    llamada a principal y error

--- javaeeCarlos/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
+++ javaeeCarlos/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
@@ -15,37 +15,51 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
-		pw.println("<br>Hola a todos: " + new java.util.Date());
-		
-		pw.println("");
-		
+		// pw.println("<br>Hola a todos: " + new java.util.Date());
+		pw.println("Si querías hacer un login, tienes que venir por post");
+
+		pw.println("Hola a todos: " + new java.util.Date());
+
 		String ruta = request.getContextPath();
+
 		pw.println(ruta);
-		
-		String nombre = request.getParameter("nombre");
-		
-		pw.append("<br>Hola ").append(nombre).toString();
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		/*
+		 * String nombre = request.getParameter("nombre");
+		 * 
+		 * pw.append("<br>Hola ").append(nombre).toString();
+		 */
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
 		String nombre = request.getParameter("nombre");
-		
-		if(nombre == null) {
-			nombre = "DESCONOCIDO";
+		String password = request.getParameter("password");
+
+		if (nombre == null || password == null) {
+			throw new RuntimeException("No se han recibido los datos de nombre y/o password");
 		}
-			PrintWriter pw= response.getWriter();
 		
-			pw.append("Hola").append(nombre);
-		
+		if ("batman".equals(nombre)&& "Iambatman".equals(password)){
+			response.sendRedirect("Principal.html");
+		}else {
+			response.sendRedirect("Error.html");
+		}
+		PrintWriter pw = response.getWriter();
+
+		pw.append("Hola").append(nombre);
 	}
 
 }
