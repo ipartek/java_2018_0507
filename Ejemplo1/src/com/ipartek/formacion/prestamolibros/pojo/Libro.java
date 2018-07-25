48f4ac4819e788716cd53485e746f20d41ba2a22
AnaMartinez <martinezdelgadoana@gmail.com>
2018-07-25 12:09:11 +0200

    arreglar

--- Ejemplo1/src/com/ipartek/formacion/prestamolibros/pojo/Libro.java
+++ Ejemplo1/src/com/ipartek/formacion/prestamolibros/pojo/Libro.java
@@ -8,10 +8,44 @@ public class Libro {
 	private String titulo;
 	private String autor;
 	private String editorial;
-	private Cliente cliente;
+	private String cliente;
 	
+	public Libro(long mock1Id, String mock1Titulo, String mock1Codigo, String mock1Editorial, String mock1Cliente) {
+		
+	id =mock1Id;
+	isbn=mock1Titulo;
+	titulo=mock1Codigo;
+	editorial=mock1Editorial;
+	cliente=mock1Cliente;
+	}
+
 	public boolean isLibre() {
 		//TODO: implementar isLibre
 		throw new RuntimeException("No implementado");
+	}
+
+	public long getId() {
+		// TODO Auto-generated method stub
+		return id;
+	}
+
+	public String getTitulo() {
+		// TODO Auto-generated method stub
+		return titulo;
+	}
+
+	public String getAutor() {
+		// TODO Auto-generated method stub
+		return autor;
+	}
+
+	public String getEditorial() {
+		// TODO Auto-generated method stub
+		return editorial;
+	}
+
+	public String getIsbn() {
+		// TODO Auto-generated method stub
+		return isbn;
 	} 
 }
