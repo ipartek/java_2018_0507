316f13c0ebd1c7b31877b65484e915840ab1686c
Carlos Ekaitz Ugalde Martinez <carlosekaitzugalde@gmail.com>
2018-07-25 09:11:32 +0200

    index html funcional

---
+++ javaeeCarlos/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.javaee.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("<br>Hola a todos: " + new java.util.Date());
+		
+		String ruta = request.getContextPath();
+		pw.println(ruta);
+		
+		String nombre = request.getParameter("nombre");
+		
+		pw.append("<br>Hola ").append(nombre).toString();
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- javaeeCarlos/src/main/webapp/index.jsp
+++ javaeeCarlos/src/main/webapp/index.jsp
@@ -1,6 +1,21 @@
 <html>
 <body>
-<h4>Adios Mundo Cruel!</h4>
-<h1>GATETES!!!</h1>
+	<!--<form action="action_page.php">
+		<div class="imgcontainer">
+			<img src="../javaeeCarlos/src/main/resources/gato.jpg" class="saludo" alt="gato" />
+		</div>
+<br><br>
+		<div class="container">
+			<label for="nombre" class="descriptor"><b>Nombre</b></label> <input
+				type="text" placeholder="Introduzca nombre" name="nombre" required>
+<br><br><br>
+			<label for="psw"><b>Contraseña</b></label> <input type="password"
+				placeholder="Introduzca contraseña" name="psw" required>
+
+			<button type="submit">Enviar</button>
+		</div>
+	</form>-->
+	<h4>Adios Mundo Cruel!</h4>
+
 </body>
 </html>
