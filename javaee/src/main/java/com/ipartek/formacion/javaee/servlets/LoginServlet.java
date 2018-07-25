bcf89f1436df7b6570e5883b0e5d34284f5ae807
Javier <jglete@gmail.com>
2018-07-25 10:04:46 +0200

    Separar funcionalidad doGet y doPost

--- javaee/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
+++ javaee/src/main/java/com/ipartek/formacion/javaee/servlets/LoginServlet.java
@@ -13,28 +13,32 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
+		
+		pw.println("Si querías hacer un login, tienes que venir por post");
+		
 		pw.println("Hola a todos: " + new java.util.Date());
 		
 		String ruta = request.getContextPath();
 		
 		pw.println(ruta);
-		
+			
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nombre = request.getParameter("nombre");
 		
 		if(nombre == null) {
 			nombre = "DESCONOCIDO";
 		}
 			
+		PrintWriter pw = response.getWriter();
+		
 		pw.append("Hola ").append(nombre);
 		
 		//pw.println("Hola " + nombre);
 		//pw.println(new StringBuilder("Hola ").append(nombre).toString());
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	
 	}
 
 }
