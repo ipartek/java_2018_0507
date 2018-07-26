94cd29835cac25fcc17a7c0dd8d49fd90751ef51
Javier <jglete@gmail.com>
2018-07-26 13:33:56 +0200

    Quitar tareas antiguas y "autogenerated stub"

--- javase/src/com/ipartek/formacion/ejemplo1/PuntoException.java
+++ javase/src/com/ipartek/formacion/ejemplo1/PuntoException.java
@@ -5,27 +5,27 @@ public class PuntoException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public PuntoException() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public PuntoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
