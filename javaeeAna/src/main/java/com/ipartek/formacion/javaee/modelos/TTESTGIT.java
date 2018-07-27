9dfa9b9d2ed457e74dcc14c48d2adac14a0f758b
AnaMartinez <martinezdelgadoana@gmail.com>
2018-07-27 08:43:41 +0200

    TESTGIT

---
+++ javaeeAna/src/main/java/com/ipartek/formacion/javaee/modelos/TTESTGIT.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.javaee.modelos;
+
+public class TTESTGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TESTGIT");
+
+	}
+
+}
