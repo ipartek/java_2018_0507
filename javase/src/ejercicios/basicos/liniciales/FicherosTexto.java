9647232cebaef8736587b4dd8916a6e61b53ae87
Aitor <Aitor@DESKTOP-3V3K7AQ>
2018-08-16 17:00:28 +0200

    Ejercicios sobre ficheros de texto

--- javase/src/ejercicios/basicos/liniciales/FicherosTexto.java
+++ javase/src/ejercicios/basicos/liniciales/FicherosTexto.java
@@ -1,15 +1,116 @@
 package ejercicios.basicos.liniciales;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.swing.JFileChooser;
 
 public class FicherosTexto {
 	
+	public static void main(String[] args) {
+		Scanner entrada = null;
+		String cadena, mayor = null, menor = null;
+		JFileChooser j = new JFileChooser();
+		j.showOpenDialog(j);
+		try {
+			String ruta = j.getSelectedFile().getAbsolutePath();
+			File f = new File(ruta);
+			entrada = new Scanner(f);
+			if(entrada.hasNext()){
+				cadena = entrada.nextLine();
+				mayor = menor = cadena;
+			}
+			while(entrada.hasNext()) {
+				cadena = entrada.nextLine();
+				if(cadena.length() > mayor.length()) {
+					mayor = cadena;
+				}else if (cadena.length() < menor.length()) {
+					menor = cadena;
+				}
+			}
+			if(mayor == null || menor == null) {
+				System.out.println("Fichero vacio");
+			}else {
+				System.out.println("Linea mas larga:");
+				System.out.println(mayor);
+				System.out.println("Linea mas corta");
+				System.out.println(menor);
+			}
+		}catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if (entrada != null) {
+				entrada.close();
+			}
+		}
+	}
+
+	
+/*	Leer fichero con StringTokenizer
+	public static void main(String[] args) {
+		File f = new File("c:/ficheros/enteros.txt");
+		int numero, suma = 0, cont = 0;
+		StringTokenizer st;
+		String cadena;
+		Scanner entrada = null;
+		try {
+		entrada = new Scanner(f);
+		while(entrada.hasNext()) {
+			cadena = entrada.nextLine();
+			st = new StringTokenizer(cadena, " ,");
+			while(st.hasMoreTokens()) {
+				numero = Integer.parseInt(st.nextToken());
+				System.out.println(numero);
+				suma = suma + numero;
+				cont++;
+			}
+		}
+		System.out.println("Numeros leidos: "+cont);
+		System.out.println("Suma "+suma);
+		}catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			entrada.close();
+		}
+	}
+*/
+	
+/*	Lectura por escaner
+	public static void main(String[] args) {
+		File f = new File("c:/ficheros/enteros.txt");
+		int numero, suma = 0, cont = 0;
+		Scanner entrada = null;
+		try {
+			entrada = new Scanner(f);
+			while(entrada.hasNextInt()) {
+				numero = entrada.nextInt();
+				System.out.println(numero);
+				suma = suma + numero;
+				cont++;
+			}
+			System.out.println("Numeros leidos: "+cont);
+			System.out.println("Suma: "+suma);
+		}catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			entrada.close();
+		}
+	}
+*/	
+
+/*Leer ficheros
+ * 
 	public static void main(String[] args) {
 		FileReader fr = null;
 		try {
@@ -36,7 +137,7 @@ public class FicherosTexto {
             }
 		}
 	}
-
+*/
 	
 /* Escribir al final del fichero.
  * 	
