0bad3bdf2e2161ddb273b9060b2155ebe3634bf6
Javier <jglete@gmail.com>
2018-09-05 14:18:21 +0200

    Corrección del examen

--- Ejemplo1/src/com/ipartek/formacion/uf2216/revista/Revista.java
+++ Ejemplo1/src/com/ipartek/formacion/uf2216/revista/Revista.java
@@ -2,22 +2,21 @@ package com.ipartek.formacion.uf2216.revista;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.ipartek.formacion.prestamolibros.pojo.Libro;
-
 
 public class Revista implements  CrudAble<Revista> {
 	
-	public String titulo;
-	public int ISBN;
-	public boolean formato;
-	public int n_paginas;
+	private String titulo;
+	private int ISBN;
+	private boolean formato;
+	private int n_paginas;
 
 	
-	private List<Revista> revista = new ArrayList<>();
+	private List<Revista> revistas = new ArrayList<>();
 	
 	
 	public Revista(int iSBN, int n_paginas, boolean formato) {
@@ -29,13 +28,14 @@ public class Revista implements  CrudAble<Revista> {
 
 	public List<Revista> getAll() {
 		// TODO Auto-generated method stub
-		return revista;
+		return revistas;
 	}
 
 	
 		
 	public boolean insertar(Revista revista) {
 		
+		this.revistas.add(revista);
 		
 		FileWriter fichero = null;
         PrintWriter pw = null;
@@ -87,21 +87,33 @@ public class Revista implements  CrudAble<Revista> {
 	@Override
 	public boolean insert(Revista pojo) {
 		
-		File fichero = new File ("c:\\temp\\lineadecodigo\\fichero.txt");
-		FileWriter fichero1 = null;
+		this.revistas.add(pojo);
+		
+		File fichero = new File ("fichero.txt");
+		
         PrintWriter pw = null;
+        FileWriter fichero1 = null;
         try{
-            
-            pw = new PrintWriter(fichero1);
+        	fichero1 = new FileWriter(fichero,true);
+            pw = new PrintWriter(fichero1,true);
 
-            for (int i = 0; i < 10; i++)
-                pw.println("pojo " + pojo.ISBN);
-	            pw.println("pojo " + pojo.n_paginas);
-	            pw.println("pojo " + pojo.titulo);
-	            pw.println("pojo " + pojo.getISBN());
-            
+            //for (int i = 0; i < 10; i++) {
+                pw.print(pojo.getISBN());
+                pw.print(";");
+	            pw.print(pojo.getn_paginas());
+	            pw.print(";");
+	            pw.print(pojo.getTitulo());
+            //}
         }catch(Exception e){
         	e.printStackTrace();
+        } finally {
+        	pw.close();
+        	try {
+				fichero1.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
 		return false;
 	}
@@ -111,18 +123,24 @@ public class Revista implements  CrudAble<Revista> {
 		System.out.println("En getRevistas para buscar la revista con id="+id);
 		//Revista resul = null;
 	
-		System.out.println("revista.size()"+revista.size());
-		while(revista.iterator() != null) {
-			System.out.println("revista.get(ISBN).toString();"+revista.get(ISBN).toString());
-			String revisbn=revista.get(ISBN).toString();
+		System.out.println("revista.size()"+revistas.size());
+		while(revistas.iterator() != null) {
+			System.out.println("revista.get(ISBN).toString();"+revistas.get(ISBN).toString());
+			String revisbn=revistas.get(ISBN).toString();
 			
 			if(revisbn.equals(id)) {
-				System.out.println("ISBN"+revista.get(ISBN).toString());
-				System.out.println("ISBN:"+revista.toString());
+				System.out.println("ISBN"+revistas.get(ISBN).toString());
+				System.out.println("ISBN:"+revistas.toString());
 			}
 		}
 		return null;
 		
 		
+	}
+
+	@Override
+	public String toString() {
+		return "Revista [titulo=" + titulo + ", ISBN=" + ISBN + ", formato=" + formato + ", n_paginas=" + n_paginas
+				+ ", revista=" + revistas + "]";
 	}
 }
