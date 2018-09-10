13cfbe9c65cc4041b5fc7c859465f8f5d08c9d4f
Aitor84 <aitor_casado@hotmail.com>
2018-09-10 09:42:04 +0200

    UF2216rec_Aitor_Casado_Arzuaga_version 1.3
    Utils y creacion de el archivo Main.

---
+++ javase/com.ipartek.formacion.uf2216rec/com/ipartek/formacion/uf2216rec/presentacion/StockMovilesMain.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.uf2216rec.presentacion;
+
+public class StockMovilesMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- javase/com.ipartek.formacion.uf2216rec/com/ipartek/formacion/uf2216rec/presentacion/Utils.java
+++ javase/com.ipartek.formacion.uf2216rec/com/ipartek/formacion/uf2216rec/presentacion/Utils.java
@@ -1,10 +1,96 @@
 package com.ipartek.formacion.uf2216rec.presentacion;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utils {
 	
 	private static Scanner s = new Scanner(System.in);
+	
+	public static String leerLinea() {
+		String linea;
+		boolean MostrarMensaje = false;
 
+		do {
+			if(MostrarMensaje) {
+				System.out.println("Introduce datos:");
+			}
+			
+			linea = s.nextLine().trim();
+			
+			MostrarMensaje = true;
+		} while( linea.length() == 0 );
+		
+		return linea;
+	}
+	
+	public static long leerLong() {
+		long l = 0;
+		boolean repetir;
+		
+		do {
+			try {
+				repetir = false;
+				l = s.nextLong();
+			} catch (InputMismatchException e) {
+				System.out.println("Por favor, introduce un número entero sin comas ni puntos");
+				s.nextLine();
+				repetir = true;
+			} catch (Exception e) {
+				System.out.println("Error no esperado");
+				throw e;
+			}
+		} while (repetir);
+		
+		s.nextLine();
+		
+		return l;
+	}
+
+	public static int leerInt() {
+		int i = 0;
+		boolean repetir;
+		
+		do {
+			try {
+				repetir = false;
+				i = s.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Por favor, introduce un número entero sin comas ni puntos");
+				s.nextLine();
+				repetir = true;
+			} catch (Exception e) {
+				System.out.println("Error no esperado");
+				throw e;
+			}
+		} while (repetir);
+		
+		s.nextLine();
+		
+		return i;
+	}
+	
+	public static int leerDouble() {
+		int i = 0;
+		boolean repetir;
+		
+		do {
+			try {
+				repetir = false;
+				i = s.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Por favor, introduce un número");
+				s.nextLine();
+				repetir = true;
+			} catch (Exception e) {
+				System.out.println("Error no esperado");
+				throw e;
+			}
+		} while (repetir);
+		
+		s.nextLine();
+		
+		return i;
+	}
 
 }
