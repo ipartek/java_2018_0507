7d67b1e21aedab6230e475cdfe460292dc0044bb
crismr <crismr_93@hotmail.com>
2018-09-12 09:19:11 +0200

    cambiar titulo

---
+++ javaee/src/main/java/com/ipartek/formacion/javaee/libros/modelos/Cabecera.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.javaee.libros.modelos;
+
+public class Cabecera {
+	private String titulo;
+
+	public Cabecera(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
