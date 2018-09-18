f3d6d44eca5d0a0c43a293b35cb1e7c7bbf0e834
Curso <Curso@LAPTOP-95OBND8K.ipartekaula.com>
2018-09-18 10:21:26 +0200

    cooki

---
+++ javaee/src/main/webapp/index.js
@@ -0,0 +1,62 @@
+/* jslint browser:true, global window, global alert */
+
+window.onload = function () {
+    'use strict';
+    
+    var form = document.getElementsByTagName("form")[0];
+    
+    //console.log(form);
+    
+    form.onsubmit = function (e) {
+        e.preventDefault();
+        
+        var hayErrores = false;
+        
+        var iNombre, iPassword;
+        iNombre = document.getElementById('nombre');
+        iPassword = document.getElementById('password');
+        
+        //console.log(iNombre, iPassword);
+        
+        if (iNombre.value.trim().length === 0) {
+            activarError(iNombre, 'Es obligatorio rellenar el nombre');
+            hayErrores = true;
+        } else {
+            desactivarError(iNombre);
+        }
+        
+        if (iPassword.value.trim().length === 0) {
+            //iPassword.style = 'border: 2px solid red';
+            activarError(iPassword, 'Es obligatorio rellenar la contraseña');
+            hayErrores = true;
+        } else {
+            //iPassword.style = '';
+            desactivarError(iPassword);
+        }
+        
+        if(!hayErrores) 
+            form.submit();
+    };
+};
+
+function activarError(objeto, texto) {
+    if(!objeto.fallo) {
+        objeto.className = 'errorCuadro'; //.style = 'border: 2px solid red';
+
+        var sError = document.createElement('span');
+        sError.className = 'errorTexto'; //.style = 'color: red';
+        sError.innerHTML = texto;
+
+        objeto.parentNode.appendChild(sError);
+
+        objeto.fallo = sError;
+    }
+}
+
+function desactivarError(objeto) {
+    if(objeto.fallo) {
+        objeto.className = '';  //.style = '';
+        objeto.parentNode.removeChild(objeto.fallo);
+        objeto.fallo = undefined;
+    }
+}
