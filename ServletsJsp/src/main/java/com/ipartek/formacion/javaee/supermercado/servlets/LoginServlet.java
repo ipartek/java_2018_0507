bcc99f35cf01220a00fb8abd258206dca17d7214
Carlos Ekaitz Ugalde Martinez <carlosekaitzugalde@gmail.com>
2018-09-20 12:32:09 +0200

    ....

--- ServletsJsp/src/main/java/com/ipartek/formacion/javaee/supermercado/servlets/LoginServlet.java
+++ ServletsJsp/src/main/java/com/ipartek/formacion/javaee/supermercado/servlets/LoginServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.ipartek.formacion.javaee.modelo.LoginForm;
+import com.ipartek.formacion.javaee.supermercado.pojo.Login;
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
