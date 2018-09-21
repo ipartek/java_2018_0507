a079c551a5593d29202e5ce1fed934ba55fb5888
AnaMartinez <martinezdelgadoana@gmail.com>
2018-09-21 10:09:42 +0200

    Filtro de usuario para pagina de administracion: Si el usuario no es nulo, puede administrar.

---
+++ javaee/src/main/java/com/ipartek/formacion/javaee/filtros/LoginFilter.java
@@ -0,0 +1,50 @@
+package com.ipartek.formacion.javaee.filtros;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.formacion.javaee.modelos.LoginForm;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+	public void destroy() {}
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest httprequest=(HttpServletRequest)request;
+		HttpSession session= httprequest.getSession();
+		
+		LoginForm usuario=(LoginForm)session.getAttribute("usuario");
+		
+		if(usuario==null) {
+			HttpServletResponse httpResponse =(HttpServletResponse)response;
+			httpResponse.sendRedirect("/javaee/loginForm.jsp");
+			return;
+		}
+		
+		//chain es la cadena de peticion que te llega cuando todo va bien para ir a ella
+		//Si no se cumple la condicion de arriba se ejecuta
+		System.out.println("*********En zona de Administrador*************");
+		chain.doFilter(request, response);
+		
+		
+		//Ejecucion
+		/*llamo a http://localhost:8080/javaee/admin
+		 * y me redirige a http://localhost:8080/javaee/loginForm.jsp*/
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {}
+
+}
