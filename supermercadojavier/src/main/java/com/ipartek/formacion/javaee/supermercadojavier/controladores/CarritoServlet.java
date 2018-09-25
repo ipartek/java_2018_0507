b87fe81bf41a8f37bacea3b17b6c6ac08177f3d3
Carlos Ekaitz Ugalde Martinez <carlosekaitzugalde@gmail.com>
2018-09-25 09:17:22 +0200

    b

---
+++ supermercadojavier/src/main/java/com/ipartek/formacion/javaee/supermercadojavier/controladores/CarritoServlet.java
@@ -0,0 +1,56 @@
+package com.ipartek.formacion.javaee.supermercadojavier.controladores;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.formacion.javaee.supermercadojavier.logica.LogicaNegocio;
+import com.ipartek.formacion.javaee.supermercadojavier.pojo.Producto;
+
+@WebServlet("/carrito")
+public class CarritoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String accion = request.getParameter("accion");
+		
+		HttpSession session = request.getSession();
+		
+		List<Producto> productos = (List<Producto>)session.getAttribute("carrito");
+		
+		if(productos == null) {
+			productos = new ArrayList<Producto>();
+			session.setAttribute("carrito", productos);
+		}
+		
+		if(id != null && accion != null) {
+			Producto producto = LogicaNegocio.obtenerProductoPorId(Long.parseLong(id));	
+			
+			switch(accion) {
+			case "add":
+				productos.add(producto);
+				break;
+			case "delete":
+				productos.remove(producto);
+				break;
+			}
+		}
+		
+		request.getRequestDispatcher("carrito.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ supermercadojavier/src/main/webapp/carrito.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ include file="/WEB-INF/includes/cabecera.jsp"%>
+
+<header>
+	<h1>Carrito</h1>
+</header>
+
+<table id="carritolistado">
+	<thead>
+		<tr>
+			<th>Nombre</th>
+			<th>Precio</th>
+		</tr>
+	</thead>
+	<tbody>
+		<c:forEach items="${carrito}" var="producto">
+			<tr>
+				<td>${producto.nombre}</td>
+				<td><fmt:formatNumber value="${producto.precio}" type="currency"/></td>
+				<td><a href="carrito?id=${producto.id}&accion=delete">Borrar</a></td>
+			</tr>
+		</c:forEach>
+	</tbody>
+</table>
+
+<%@ include file="/WEB-INF/includes/pie.jsp"%>
