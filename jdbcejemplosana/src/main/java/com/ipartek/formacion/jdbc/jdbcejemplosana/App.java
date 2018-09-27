f12fc4b333def2c37d3aec2bec9db77e1529bdb4
Curso <Curso@PORT-Z5.ipartekaula.com>
2018-09-27 14:21:10 +0200

    conexion a BBDD y consultas en proyecto maven con jdbc try catch y pom dependencias con java 1.7 en adelante

--- jdbcejemplosana/src/main/java/com/ipartek/formacion/jdbc/jdbcejemplosana/App.java
+++ jdbcejemplosana/src/main/java/com/ipartek/formacion/jdbc/jdbcejemplosana/App.java
@@ -1,50 +1,65 @@
 package com.ipartek.formacion.jdbc.jdbcejemplosana;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws SQLException
-    {
-    	
-   
-        String url="jdbc:mysql://localhost:3307/ipartek";
-        String usuario= "root";
-        String password= "admin";
-        
-        
-        //conseguir conexion
-        Connection conn = DriverManager.getConnection(url, usuario, password);
-        Statement stmt = conn.createStatement();
-        
-        //consulta
-        String sql="SELECT * FROM usuarios";
-        
-        ResultSet rs= stmt.executeQuery(sql);
-        
-        rs.next();
-        
-        System.out.println(rs.getString(2));
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		
+		// url de y para la conexion a la bbdd, usuario y contraseña
+		String url = "jdbc:mysql://localhost:3307/ipartek?serverTimezone=UTC&useSSL=false";
+		String usuario = "root";
+		String password = "admin";
+		// conseguir conexion pasandole los datos anteriores (url de y para la conexion
+		// a la bbdd, usuario y contraseña)
+		try (Connection conn = DriverManager.getConnection(url, usuario, password)) {
+
+			try (Statement stmt = conn.createStatement()) {
+				// consulta
+				String sql = "SELECT id, email, password FROM usuarios";
+
+				try (ResultSet rs = stmt.executeQuery(sql)) {
+
+					
+					// extra titulares (no lo haremos)
+					// Columnas desde metadatos
+					//ResultSetMetaData rsmd = rs.getMetaData();
+					//int columnas = rsmd.getColumnCount();
+
+					//for (int i = 1; i <= columnas; i++) {
+					//	System.out.print(rsmd.getColumnName(i) + '\t');
+					//}
+
+					//System.out.println();
+					// Fin
+					// mientras haya resultado que saque 
+					while (rs.next()) {
+						System.out.printf("%s\t%s\t%s\n", rs.getLong("id"), rs.getString("email"), rs.getString("password"));
+					}
+				} catch (Exception e) {
+					System.out.println("ERROR AL CREAR EL RESULTSET");
+				} 
+			} catch (Exception e) {
+				System.out.println("ERROR AL CREAR LA SENTENCIA");
+			} 
+			
+		} catch (SQLException e) {
+			System.out.println("ERROR DE CONEXION");
+			System.out.println(e.getMessage());
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
