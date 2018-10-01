6df9c4b37d466922e8b9d5d84e0e2660b12b78f2
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-10-01 13:21:06 +0200

    xczxc

---
+++ youtube/src/main/java/com/ipartek/formacion/youtube/model/AccesoDatosEception.java
@@ -0,0 +1,5 @@
+package com.ipartek.formacion.youtube.model;
+
+public class AccesoDatosEception extends Exception {
+
+}
--- youtube/src/main/java/com/ipartek/formacion/youtube/model/VideoArrayListDAO.java
+++ youtube/src/main/java/com/ipartek/formacion/youtube/model/VideoArrayListDAO.java
@@ -1,78 +1,98 @@
 package com.ipartek.formacion.youtube.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 import com.ipartek.formacion.youtube.Video;
 
 public class VideoArrayListDAO implements CrudAble<Video> {
-
-	private static VideoArrayListDAO INSTANCE = null;
-	private static List<Video> videos = null;
-
+	String url = "jdbc:mysql://localhost:3307/ipartek?serverTimezone=UTC&useSSL=false";
+	String usuario = "root";
+	String password = "admin";
+	
 	private VideoArrayListDAO() {
-		videos = new ArrayList<Video>();
-		try {
-			videos.add(new Video("LPDhuthFD98", "Surf Search Spot 2 0 video promo"));
-			videos.add(new Video("a9WnQFI8jQU", "Betagarri - Sweet Mary"));
-			videos.add(new Video("0sLK1SKfItM", "Su Ta Gar - Begira"));
-		} catch (Exception e) {
+
+	}
+
+	@Override
+	public boolean insert(Video pojo) {
+
+		String id = "";
+	}
+
+	@Override
+	public List<Video> getAll() {
+		
+		
+		List<Video> videos = new ArrayList<>();
+		Video vi = new Video();
+			
+			try (Connection conn = DriverManager.getConnection(url, usuario, password)) {
+				String sql ="SELECT id, nombre FROM videos";
+				 try (Statement pst = conn.prepareStatement(sql)) {
+				
+				 }
+			           try(ResultSet rs = pst.executeQuery()){
+			            while(rs.next()) {
+			                vi.setId(rs.getString("id"));
+			                vi.setNombre(rs.getString("nombre"));
+			                videos.add(vi);
+			            	}
+			} catch (Exception e) {
+				  throw new AccesoDatosEception(e.getMessage(),e);
+			}
+	           
+	        catch(Exception e) {
+	               throw new AccesoDatosEception(e.getMessage(),e);
+	        
+	        }
+			catch(SQLException e) {
+				  throw new AccesoDatosEception(e.getMessage(),e);
+        }
+	return videos;
+	}
+	
+
+	@Override
+	public Video getById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Video pojo) {
+		long id;
+		String nuevoNom;
+
+		String sqlu = "UPDATE videos SET nombre = ? WHERE id = ?";
+
+		try (PreparedStatement pst = conn.prepareStatement(sqlu)) {
+
+			pst.setLong(2, id); // el 2 es a la interrogacion que hago referencia en la sentencia SQL arriba
+			pst.setString(1, nuevoNom);
+
+			int numFilas = pst.executeUpdate();
+
+			if (numFilas != 1) {
+				System.out.println("El número de filas modificado ha sido " + numFilas);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
-	public static synchronized VideoArrayListDAO getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new VideoArrayListDAO();
-		}
-
-		return INSTANCE;
-	}
-
 	@Override
-	public boolean insert(Video pojo) {
-		return videos.add(pojo);
-	}
-
-	@Override
-	public List<Video> getAll() {
-		return videos;
-	}
-
-	@Override
-	public Video getById(String id) {
-		Video resul = null;
-		if (id != null) {
-			for (Video v : videos) {
-				if (id.equals(v.getId())) {
-					resul = v;
-				}
-			}
-		}
-		return resul;
-	}
-
-	@Override
-	public boolean update(Video pojo) {
+	public boolean delete(String id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	@Override
-	public boolean delete(String id) {
-		boolean resul = false;
-		Video v = null;
-		if ( id != null ) { 
-			for (int i = 0; i < videos.size(); i++) {
-				v = videos.get(i); 
-				if (id.equals(v.getId()) ) { 
-					resul = videos.remove(v);
-					break;
-				}
-			}
-		}	
-		return resul;
-	}
-
-}
