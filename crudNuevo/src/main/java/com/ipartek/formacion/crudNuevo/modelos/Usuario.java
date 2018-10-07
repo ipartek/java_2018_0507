cd4cc04a9808273737008fe9e4941f94ecbd9c48
crismr <crismr_93@hotmail.com>
2018-10-07 21:51:08 +0200

    Add files via upload

---
+++ crudNuevo/src/main/java/com/ipartek/formacion/crudNuevo/modelos/Usuario.java
@@ -0,0 +1,117 @@
+package com.ipartek.formacion.crudNuevo.modelos;
+
+public class Usuario {
+	private Long id = -1L;
+	private String email = "", password = "";
+	private String errorEmail = "", errorPassword = "";
+	private boolean correcto = true;
+	
+	public Usuario(long id, String email, String password) {
+		super();
+		this.id = id;
+		setEmail(email);
+		setPassword(password);
+	}
+	public Usuario(String email, String password) {
+		this(-1L, email, password);
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		if(email == null) {
+			setErrorEmail("Email nulo");
+			setCorrecto(false);
+			return;
+		}
+		
+		if(!email.matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]+)$")) {
+			setErrorEmail("No coincide con el formato de email");
+			setCorrecto(false);
+			return;
+		}
+		
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		if(password == null) {
+			setErrorPassword("No se admite un password nulo");
+			setCorrecto(false);
+			return;
+		}
+		
+		if(password.trim().length() < 8) {
+			setErrorPassword("No se admite un password menor de 8 caracteres");
+			setCorrecto(false);
+			return;
+		}
+		
+		this.password = password;
+	}
+	
+	public String getErrorEmail() {
+		return errorEmail;
+	}
+	public void setErrorEmail(String errorEmail) {
+		this.errorEmail = errorEmail;
+	}
+	public String getErrorPassword() {
+		return errorPassword;
+	}
+	public void setErrorPassword(String errorPassword) {
+		this.errorPassword = errorPassword;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", email=" + email + ", password=" + password + "]";
+	}
+	public boolean isCorrecto() {
+		return correcto;
+	}
+	private void setCorrecto(boolean correcto) {
+		this.correcto = correcto;
+	}
+	
+	
+}
---
+++ crudNuevo/src/main/webapp/WEB-INF/admin/includes/cabecera.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Administración</title>
+
+<!-- Fontawesome -->
+<link rel="stylesheet"
+	href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+	integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+	crossorigin="anonymous">
+
+<!-- Bootstrap core CSS -->
+<link
+	href="https://blackrockdigital.github.io/startbootstrap-shop-item/vendor/bootstrap/css/bootstrap.min.css"
+	rel="stylesheet">
+
+</head>
+<body class="container">
+
+	<header class="jumbotron">
+		<h1>VideoYoutube</h1>
+	</header>
