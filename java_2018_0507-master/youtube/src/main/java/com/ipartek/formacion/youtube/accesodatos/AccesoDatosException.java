94bc217481bfe3d085eaf4aebfba45272ba5d7c2
Ana Martínez Delgado <martinezdelgadoana@gmail.com>
2018-10-19 13:24:58 +0200

    JAVI

---
+++ java_2018_0507-master/youtube/src/main/java/com/ipartek/formacion/youtube/accesodatos/AccesoDatosException.java
@@ -0,0 +1,27 @@
+package com.ipartek.formacion.youtube.accesodatos;
+
+public class AccesoDatosException extends RuntimeException {
+
+	private static final long serialVersionUID = 3742506394010182266L;
+
+	public AccesoDatosException() {
+	}
+
+	public AccesoDatosException(String message) {
+		super(message);
+	}
+
+	public AccesoDatosException(Throwable cause) {
+		super(cause);
+	}
+
+	public AccesoDatosException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AccesoDatosException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
