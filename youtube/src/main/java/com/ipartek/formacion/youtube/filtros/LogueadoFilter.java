e6fea1c01eae2ffead80af23daed45165a348cce
Javier Lete <jglete@gmail.com>
2018-10-22 13:30:55 +0200

    Filtro de seguridad de logueado para gestión y datos de videos

---
+++ youtube/src/main/java/com/ipartek/formacion/youtube/filtros/LogueadoFilter.java
@@ -0,0 +1,44 @@
+package com.ipartek.formacion.youtube.filtros;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD
+		}
+					, urlPatterns = { "/video", "/gestionvideos" })
+public class LogueadoFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		
+		if(session.getAttribute("usuario") == null) {
+			request.setAttribute("alertatipo", "danger");
+			request.setAttribute("alertatexto", "No puedes acceder a esa opción sin estar logueado");
+			
+			request.getRequestDispatcher("/").forward(request, response);
+			
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+	public void destroy() {
+	}
+}
