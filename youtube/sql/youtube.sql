09ef8282e2603f698dba18e4f5c341ba9c95bd47
Javier Lete <jglete@gmail.com>
2018-10-23 12:07:55 +0200

    Cambio en base de datos para puntuaciones

--- youtube/sql/youtube.sql
+++ youtube/sql/youtube.sql
@@ -24,10 +24,10 @@ DROP TABLE IF EXISTS `comentarios`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `comentarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` datetime NOT NULL,
   `id_videos` int(11) NOT NULL,
   `id_usuarios` int(11) NOT NULL,
   `texto` text NOT NULL,
+  `fecha` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_comentarios_videos1_idx` (`id_videos`),
   KEY `fk_comentarios_usuarios1_idx` (`id_usuarios`),
@@ -42,7 +42,7 @@ CREATE TABLE `comentarios` (
 
 LOCK TABLES `comentarios` WRITE;
 /*!40000 ALTER TABLE `comentarios` DISABLE KEYS */;
-INSERT INTO `comentarios` VALUES (1,'2018-10-23 08:00:00',11,2,'Este video está de p*** madre'),(2,'2018-10-23 08:01:00',11,1,'Qué dices, es más malo que la peste'),(3,'2018-10-22 00:00:00',12,1,'No entiendo como nadie ha comentado esta maravilla de video'),(4,'2018-10-23 08:15:00',11,1,'Y una m****. Mola un montón y tú eres un p*** g******** de m******'),(5,'2018-10-23 08:50:41',11,1,'Admin es tonto... JAJAJAJAJAJA'),(6,'2018-10-23 08:52:55',11,1,'Admin es tonto... JAJAJAJAJAJA');
+INSERT INTO `comentarios` VALUES (1,11,2,'Este video está de p*** madre','2018-10-23 08:00:00'),(2,11,1,'Qué dices, es más malo que la peste','2018-10-23 08:01:00'),(3,12,1,'No entiendo como nadie ha comentado esta maravilla de video','2018-10-22 00:00:00'),(4,11,1,'Y una m****. Mola un montón y tú eres un p*** g******** de m******','2018-10-23 08:15:00'),(5,11,1,'Admin es tonto... JAJAJAJAJAJA','2018-10-23 08:50:41'),(6,11,1,'Admin es tonto... JAJAJAJAJAJA','2018-10-23 08:52:55');
 /*!40000 ALTER TABLE `comentarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,6 +70,35 @@ LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` VALUES (1,'javier@email.com','yepa'),(2,'admin@email.com','nuevapassword');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios_puntua_videos`
+--
+
+DROP TABLE IF EXISTS `usuarios_puntua_videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `usuarios_puntua_videos` (
+  `usuarios_id` int(11) NOT NULL,
+  `videos_id` int(11) NOT NULL,
+  `puntuacion` tinyint(4) NOT NULL,
+  PRIMARY KEY (`usuarios_id`,`videos_id`),
+  KEY `fk_usuarios_has_videos_videos1_idx` (`videos_id`),
+  KEY `fk_usuarios_has_videos_usuarios1_idx` (`usuarios_id`),
+  CONSTRAINT `fk_usuarios_has_videos_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `fk_usuarios_has_videos_videos1` FOREIGN KEY (`videos_id`) REFERENCES `videos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios_puntua_videos`
+--
+
+LOCK TABLES `usuarios_puntua_videos` WRITE;
+/*!40000 ALTER TABLE `usuarios_puntua_videos` DISABLE KEYS */;
+INSERT INTO `usuarios_puntua_videos` VALUES (1,11,3),(1,12,1),(2,11,5),(2,12,5);
+/*!40000 ALTER TABLE `usuarios_puntua_videos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -110,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-23 11:47:16
+-- Dump completed on 2018-10-23 12:02:22
