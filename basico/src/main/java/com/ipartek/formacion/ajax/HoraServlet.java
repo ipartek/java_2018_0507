9dc743ffe49a6cc23df99a62b7c2c47a7735c3bd
Javier Lete <jglete@gmail.com>
2018-10-31 14:30:01 +0100

    Proyecto AJAX básico de horas y libros

---
+++ basico/src/main/java/com/ipartek/formacion/ajax/HoraServlet.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.ajax;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HoraServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter()
+				.append("{ \"hora\": \"" + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + " - " + new Date().getTimezoneOffset() + "\" }");
+	}
+
+}
