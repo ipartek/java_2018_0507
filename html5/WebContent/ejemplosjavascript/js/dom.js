a16e5aee1680f0dd1eb4009b8dad2ccfd20f7382
JavierLete <JavierLete@10.0.2.15>
2018-11-06 09:27:40 +0100

    Procesar varios párrafos

--- html5/WebContent/ejemplosjavascript/js/dom.js
+++ html5/WebContent/ejemplosjavascript/js/dom.js
@@ -3,3 +3,11 @@ function cambiarTitulo() {
 
 	h1.innerText = 'Título cambiado desde JavaScript';
 }
+
+function modificarParrafos() {
+	var parrafos = document.getElementsByTagName('p');
+	
+	for(var i = 0; i < parrafos.length; i++) {
+		parrafos[i].innerText = 'Párrafo modificado ' + i;
+	}
+}
