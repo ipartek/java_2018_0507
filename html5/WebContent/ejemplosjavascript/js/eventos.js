7d64026b5d4de120a0c8b62f3cb0d60b64f0622f
JavierLete <JavierLete@10.0.2.15>
2018-11-07 09:14:32 +0100

    Creación de botones desde JS con eventos incluidos

--- html5/WebContent/ejemplosjavascript/js/eventos.js
+++ html5/WebContent/ejemplosjavascript/js/eventos.js
@@ -1,4 +1,24 @@
 window.onload = function () {
+	var botonera = document.getElementById('botonera');
+	
+	while (botonera.firstChild) {
+		botonera.removeChild(botonera.firstChild);
+	}
+	
+	for(let i = 0; i <= 9; i++) {
+		var boton = document.createElement('button');
+		
+		boton.innerHTML = i;
+		
+		boton.onclick = function () {
+			alert(this.innerHTML);
+		};
+		
+		botonera.appendChild(boton);
+	}
+};
+/*
+window.onload = function () {
 	var botones = document.getElementById('botonera').getElementsByTagName('button'); //.childNodes
 	
 	for(let i = 0; i < botones.length; i++) {
@@ -7,3 +27,4 @@ window.onload = function () {
 		};
 	}
 };
+*/
