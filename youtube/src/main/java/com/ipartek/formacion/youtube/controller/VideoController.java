87a6a51967c31b7b4f619df1dee860504146b4d0
Peybool <pablomartin9696@gmail.com>
2018-11-09 12:09:51 +0100

    asdasd

--- youtube/src/main/java/com/ipartek/formacion/youtube/controller/VideoController.java
+++ youtube/src/main/java/com/ipartek/formacion/youtube/controller/VideoController.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.youtube.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ public class VideoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.getRequestDispatcher("/WEB-INF/vistas/video.jsp").forward(request, response);
 	}
 
