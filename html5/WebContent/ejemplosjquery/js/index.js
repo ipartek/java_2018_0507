acd7555acd1dfc4055b4718356528a1c93e051c4
JavierLete <JavierLete@10.0.2.15>
2018-11-09 10:08:32 +0100

    Ejemplos básicos de jQuery

---
+++ html5/WebContent/ejemplosjquery/js/index.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('button:contains(2),button:contains(3)').click(function() {
+		alert(this.innerHTML);
+	});
+	
+	$('p').each(function() {
+		console.log(this.innerHTML);
+	});
+	
+	$('form').submit(function(e) {
+		e.preventDefault();
+		
+		console.log($('input[name=nombre]').val());
+		console.log($('input[name=nombre]').attr('type'));
+		console.log($('input[name=nombre]')[0].value);
+		console.log($('input[name=nombre]')[0].type);
+	});
+});
