2bea126a66f553a961d39294aa04d15acd677f0e
Ana Martínez Delgado <martinezdelgadoana@gmail.com>
2018-11-09 13:40:13 +0100

    dodelete...

---
+++ ajax/src/main/java/com/ipartek/formacion/ajax/LibrosServlet.java
@@ -0,0 +1,78 @@
+package com.ipartek.formacion.ajax;
+
+import java.io.IOException;
+import java.util.TreeMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@WebServlet("/libros/*")
+public class LibrosServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static TreeMap<Long, Libro> libros = new TreeMap<Long, Libro>();
+	
+	static {
+		libros.put(1L, new Libro(1L, "Libro1"));
+		libros.put(2L, new Libro(2L, "Libro2"));
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+		String path = request.getPathInfo();
+		
+		Long id = path == null ? null : Long.parseLong(path.replace("/", ""));
+		
+		Gson gson = new Gson();
+		
+		if(id == null) {
+			response.getWriter().append(gson.toJson(libros.values()));
+		} else {
+			response.getWriter().append(gson.toJson(libros.get(id)));
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String json = request.getParameter("libro");
+		
+		Gson gson = new Gson();
+		
+		System.out.println(json);
+		
+		Libro libro = gson.fromJson(json, Libro.class);
+		
+		libro.setId(libros.lastKey() + 1);
+		
+		System.out.println(libro);
+		
+		libros.put(libro.getId(), libro);
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String json = request.getParameter("libro");
+		
+		Gson gson = new Gson();
+		
+		System.out.println(json);
+		
+		Libro libro = gson.fromJson(json, Libro.class);
+		
+		System.out.println(libro);
+		
+		libros.put(libro.getId(), libro);
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getPathInfo();
+		
+		Long id = path == null ? null : Long.parseLong(path.replace("/", ""));
+		
+		libros.remove(id);
+	}
+}
--- ajax/src/main/webapp/index.jsp
+++ ajax/src/main/webapp/index.jsp
@@ -1,5 +1,32 @@
+<!DOCTYPE html>
 <html>
+<head>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"
+	integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+	crossorigin="anonymous"></script>
+<script src="js/index.js"></script>
+</head>
 <body>
-<h2>Hello World!</h2>
+
+	<table id="tablalibros">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Nombre</th>
+				<th>Opciones</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- <tr><th>id</th><td>nombre</td><td><a href="javascript:mostrarLibro(id)">Editar</a><tr> -->
+		</tbody>
+	</table>
+	
+	<form id="formulariolibros">
+		<span></span>
+		<input type="hidden" name="id" id="id" />
+		<input name="nombre" id="nombre" />
+		<button>Aceptar</button>
+	</form>
+
 </body>
 </html>
