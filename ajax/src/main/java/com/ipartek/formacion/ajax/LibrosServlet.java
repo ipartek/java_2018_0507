3e68969ca06305f32698cb88a50896c5aad841c4
Ana Martínez Delgado <martinezdelgadoana@gmail.com>
2018-11-12 14:12:29 +0100

    Ajax

--- ajax/src/main/java/com/ipartek/formacion/ajax/LibrosServlet.java
+++ ajax/src/main/java/com/ipartek/formacion/ajax/LibrosServlet.java
@@ -16,63 +16,72 @@ public class LibrosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static TreeMap<Long, Libro> libros = new TreeMap<Long, Libro>();
-	
+
 	static {
 		libros.put(1L, new Libro(1L, "Libro1"));
 		libros.put(2L, new Libro(2L, "Libro2"));
 	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private static Gson gson = new Gson();
+
+	// GET /libros
+	// GET /libros/5
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("application/json");
-		
-		String path = request.getPathInfo();
-		
-		Long id = path == null ? null : Long.parseLong(path.replace("/", ""));
-		
-		Gson gson = new Gson();
-		
-		if(id == null) {
+
+		Long id = getIdFromUrl(request);
+
+		if (id == null) {
 			response.getWriter().append(gson.toJson(libros.values()));
 		} else {
-			response.getWriter().append(gson.toJson(libros.get(id)));
+			Libro libro = libros.get(id);
+
+			if (libro == null) {
+				response.sendError(404, "No se ha encontrado el libro especificado cuyo id es " + id);
+			} else {
+				response.getWriter().append(gson.toJson(libro));
+			}
+
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String json = request.getParameter("libro");
-		
-		Gson gson = new Gson();
-		
-		System.out.println(json);
-		
-		Libro libro = gson.fromJson(json, Libro.class);
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Libro libro = gson.fromJson(request.getReader(), Libro.class);
+
 		libro.setId(libros.lastKey() + 1);
-		
-		System.out.println(libro);
-		
+
 		libros.put(libro.getId(), libro);
 	}
 
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String json = request.getParameter("libro");
-		
-		Gson gson = new Gson();
-		
-		System.out.println(json);
-		
-		Libro libro = gson.fromJson(json, Libro.class);
-		
-		System.out.println(libro);
-		
-		libros.put(libro.getId(), libro);
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Libro libro = gson.fromJson(request.getReader(), Libro.class);
+
+		if(libros.containsKey(libro.getId())) {
+			libros.put(libro.getId(), libro);
+		} else {
+			response.sendError(500, "No se puede actualizar un libro NO existente");
+		}
 	}
-	
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getPathInfo();
-		
-		Long id = path == null ? null : Long.parseLong(path.replace("/", ""));
-		
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Long id = getIdFromUrl(request);
+
+		if(libros.containsKey(id)) {
 		libros.remove(id);
+		} else {
+			response.sendError(500, "No puedo borrar un libro que no existe");
+		}
+	}
+
+	private Long getIdFromUrl(HttpServletRequest request) {
+		if (request.getPathInfo() == null || request.getPathInfo().equals("/")) {
+			return null;
+		}
+
+		return Long.parseLong(request.getPathInfo().split("/")[1]);
 	}
 }
--- ajax/src/main/webapp/js/index.js
+++ ajax/src/main/webapp/js/index.js
@@ -1,42 +1,123 @@
 $(function() {
-	$('#formulariolibros').hide();
+	ocultarFormulario();
 	
-	var jqxhr = $.ajax("libros")
-		.done(rellenarTabla)
-		.fail(function() { alert("error"); })
-		.always(function() { console.log("complete"); });
+	$('#formulariolibros').submit(enviarFormulario);
+	
+	$('#botoncancelar').click(function(e) {
+		e.preventDefault();
+		
+		ocultarFormulario();
+	});
+	
+	$('#enlaceNuevoLibro').click(function(e) {
+		e.preventDefault();
+		
+		mostrarFormulario();
+	});
+	
+	pedirLibros();
 });
 
-function rellenarTabla(libros) {
-	console.log("success");
-	
-	$(libros).each(function() {
-		$('<tr><th>' + this.id + '</th><td>' + this.nombre + '</td>' +
-				'<td><a href="javascript:mostrarLibro('+ this.id + ')">Editar</a></td></tr>')
-		.appendTo($('#tablalibros > tbody'));
-		
-		/*
-		$(`
-		<tr>
-			<th>${this.id}</th>
-			<td>${this.nombre}</td>
-			<td><a href="javascript:mostrarLibro(${this.id})">Editar</a></td>
-		</tr>
-		`).appendTo($('#tablalibros > tbody'));
-		*/
+function pedirLibros() {
+	$.ajax("libros").done(rellenarTabla).fail(function() { //Al cargar rellenar libros
+		alert("error");
+	}).always(function() {
+		console.log("complete");
 	});
 }
 
+function rellenarTabla(libros) {
+	console.log("success");
+
+	var $tablalibrosbody = $('#tablalibros > tbody');
+
+	$tablalibrosbody.empty();
+
+	$(libros).each(  //Por cada libro mostrar los links de editar y borrar
+			function() {
+				$(
+						'<tr><th>' + this.id + '</th><td>' + this.nombre
+								+ '</td>'
+								+ '<td><a href="javascript:mostrarLibro('
+								+ this.id + ')">Editar</a> '
+								+ '<a href="javascript:borrarLibro(' + this.id
+								+ ')">Borrar</td></tr>').appendTo(
+						$tablalibrosbody);
+
+				/*
+				 * $(` <tr> <th>${this.id}</th> <td>${this.nombre}</td> <td><a
+				 * href="javascript:mostrarLibro(${this.id})">Editar</a></td>
+				 * </tr> `).appendTo($('#tablalibros > tbody'));
+				 */
+			});
+}
+
 function mostrarLibro(id) {
-	var jqxhr = $.ajax("libros/" + id)
-	.done(rellenarFormulario)
-	.fail(function() { alert("error"); })
-	.always(function() { console.log("complete"); });
+	$.ajax("libros/" + id).done(rellenarFormulario).fail(function() {  //Error al mostrar el libro con ese id
+		alert("error");
+	}).always(function() {
+		console.log("complete");
+	});
 }
 
 function rellenarFormulario(libro) {
-	$('#formulariolibros').show();
-	
+	mostrarFormulario();
+
 	$('#id').val(libro.id);
 	$('#nombre').val(libro.nombre);
+}
+
+function enviarFormulario(e) {
+	e.preventDefault();  //Evitar el acion del form
+
+	var vlibro = {
+		id : parseInt($('#id').val()),  //Convertir a int para que el servlet no falle
+		nombre : $('#nombre').val()
+	};
+
+	console.log(vlibro);
+
+	$.ajax({
+		method : (vlibro.id ? 'PUT' : 'POST'), //Si id == 0 se hace POST y si no PUT
+		url : 'libros' + (vlibro.id ? '/' + vlibro.id : ''), //Si id != 0 se agrega /id
+		data : JSON.stringify(vlibro)
+	}).done(function() {
+		ocultarFormulario();
+		pedirLibros();
+	}).fail(function() {
+		alert("error");
+	}).always(function() {
+		console.log("complete");
+	});
+}
+
+function borrarLibro(id) {
+	if (confirm('¿Estás seguro de que quieres borrar el libro cuyo id es ' + id
+			+ '?')) {
+
+		$.ajax({
+			method : 'DELETE',
+			url : 'libros/' + id
+		}).done(function() {
+			pedirLibros();
+		}).fail(function() {
+			alert("error");
+		}).always(function() {
+			console.log("complete");
+		});
+	} else {
+		alert('No se borrará el libro');
+	}
+}
+
+function mostrarFormulario() {
+	$('#enlaceNuevoLibro').hide();
+	$('#formulariolibros').show();
+}
+
+function ocultarFormulario() {
+	$('#enlaceNuevoLibro').show(); // mostrar boton de crear libro
+	$('#formulariolibros').hide();  //ocultar formulario
+	$('#formulariolibros').trigger('reset');  //lanza el evento reset -- el evento reset te los pone a valor por defecto
+	$('#id').val(0);
 }
