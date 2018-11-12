65559b72b4a44f824ed89fa4e490181c77894e8f
JavierLete <JavierLete@10.0.2.15>
2018-11-12 12:59:31 +0100

    Borrar libro

--- ajax/src/main/webapp/js/index.js
+++ ajax/src/main/webapp/js/index.js
@@ -1,68 +1,97 @@
 $(function() {
 	$('#formulariolibros').hide().submit(enviarFormulario);
-	
+
 	pedirLibros();
 });
 
 function pedirLibros() {
-	var jqxhr = $.ajax("libros")
-	.done(rellenarTabla)
-	.fail(function() { alert("error"); })
-	.always(function() { console.log("complete"); });
+	$.ajax("libros").done(rellenarTabla).fail(function() {
+		alert("error");
+	}).always(function() {
+		console.log("complete");
+	});
 }
 
 function rellenarTabla(libros) {
 	console.log("success");
-	
+
 	var $tablalibrosbody = $('#tablalibros > tbody');
-	
+
 	$tablalibrosbody.empty();
-	
-	$(libros).each(function() {
-		$('<tr><th>' + this.id + '</th><td>' + this.nombre + '</td>' +
-				'<td><a href="javascript:mostrarLibro('+ this.id + ')">Editar</a> '+
-				'<a href="javascript:borrarLibro(' + this.id + ')">Borrar</td></tr>')
-		.appendTo($tablalibrosbody);
-		
-		/*
-		$(`
-		<tr>
-			<th>${this.id}</th>
-			<td>${this.nombre}</td>
-			<td><a href="javascript:mostrarLibro(${this.id})">Editar</a></td>
-		</tr>
-		`).appendTo($('#tablalibros > tbody'));
-		*/
-	});
+
+	$(libros).each(
+			function() {
+				$(
+						'<tr><th>' + this.id + '</th><td>' + this.nombre
+								+ '</td>'
+								+ '<td><a href="javascript:mostrarLibro('
+								+ this.id + ')">Editar</a> '
+								+ '<a href="javascript:borrarLibro(' + this.id
+								+ ')">Borrar</td></tr>').appendTo(
+						$tablalibrosbody);
+
+				/*
+				 * $(` <tr> <th>${this.id}</th> <td>${this.nombre}</td> <td><a
+				 * href="javascript:mostrarLibro(${this.id})">Editar</a></td>
+				 * </tr> `).appendTo($('#tablalibros > tbody'));
+				 */
+			});
 }
 
 function mostrarLibro(id) {
-	var jqxhr = $.ajax("libros/" + id)
-	.done(rellenarFormulario)
-	.fail(function() { alert("error"); })
-	.always(function() { console.log("complete"); });
+	$.ajax("libros/" + id).done(rellenarFormulario).fail(function() {
+		alert("error");
+	}).always(function() {
+		console.log("complete");
+	});
 }
 
 function rellenarFormulario(libro) {
 	$('#formulariolibros').show();
-	
+
 	$('#id').val(libro.id);
 	$('#nombre').val(libro.nombre);
 }
 
 function enviarFormulario(e) {
 	e.preventDefault();
-	
-	var vlibro = { id: $('#id').val(), nombre: $('#nombre').val() };
-	
+
+	var vlibro = {
+		id : $('#id').val(),
+		nombre : $('#nombre').val()
+	};
+
 	console.log(vlibro);
-	
-	var jqxhr = $.ajax({
-		method: 'PUT',
-		url: 'libros',
-		data: JSON.stringify(vlibro)
-	})
-	.done(function() { $('#formulariolibros').hide(); pedirLibros(); })
-	.fail(function() { alert("error"); })
-	.always(function() { console.log("complete"); });
+
+	$.ajax({
+		method : 'PUT',
+		url : 'libros',
+		data : JSON.stringify(vlibro)
+	}).done(function() {
+		$('#formulariolibros').hide();
+		pedirLibros();
+	}).fail(function() {
+		alert("error");
+	}).always(function() {
+		console.log("complete");
+	});
+}
+
+function borrarLibro(id) {
+	if (confirm('¿Estás seguro de que quieres borrar el libro cuyo id es ' + id
+			+ '?')) {
+
+		$.ajax({
+			method : 'DELETE',
+			url : 'libros/' + id
+		}).done(function() {
+			pedirLibros();
+		}).fail(function() {
+			alert("error");
+		}).always(function() {
+			console.log("complete");
+		});
+	} else {
+		alert('No se borrará el libro');
+	}
 }
