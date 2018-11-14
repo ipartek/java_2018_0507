a7dc9580064488e95c61e394e9ce5aea5a5496e6
JavierLete <JavierLete@10.0.2.15>
2018-11-14 08:48:27 +0100

    Proyecto prestamolibros con Spring

---
+++ prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/App.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.prestamolibrosspring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
