8fd0e4b146b437c7f46164d85fba49fcd420fbc1
JavierLete <JavierLete@10.0.2.15>
2018-11-14 12:28:13 +0100

    Versión final de CRUD con MVC

--- prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/controllers/EditorialController.java
+++ prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/controllers/EditorialController.java
@@ -1,9 +1,13 @@
 package com.ipartek.formacion.prestamolibrosspring.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ipartek.formacion.prestamolibrosspring.model.Editorial;
 import com.ipartek.formacion.prestamolibrosspring.repositories.EditorialMapRepository;
@@ -24,21 +28,47 @@ public class EditorialController {
 		return "editoriales";
 	}
 	
-	@GetMapping("/editoriales/crear") 
-	public String mostrarPantallaCrear() {
-		return "editorial";
-	}
-
 	@GetMapping("/editoriales/{accion}/{id}") 
-	public String mostrarPantallaEditar(
-			@PathVariable(value="id") Long id, @PathVariable(value="accion") String accion, Model modelo) {
+	public String mostrarPantalla(
+			@PathVariable("id") Long id, @PathVariable("accion") String accion, Model modelo) {
 		
 		modelo.addAttribute("id", id);
 		modelo.addAttribute("accion", accion);
 		
-		modelo.addAttribute("editorial", repositorio.getById(id));
+		Editorial editorial;
+		
+		if(id != null && id != 0L) {
+			editorial = repositorio.getById(id);
+		} else {
+			editorial = new Editorial(0L, "");
+		}
+		
+		modelo.addAttribute("editorial", editorial);
 		
 		return "editorial";
 	}
+	
+	@PostMapping("/editoriales/{accion}")
+	public String procesarPantalla(
+			@PathVariable(value="accion") String accion, 
+			@Valid Editorial editorial, BindingResult bindingResult) {
+		
+		if (!accion.equals("borrar") && bindingResult.hasErrors()) {
+            return "editorial";
+        }
 
+		switch(accion) {
+		case "crear": 
+			repositorio.insert(editorial);
+			break;
+		case "editar":
+			repositorio.update(editorial);
+			break;
+		case "borrar":
+			repositorio.delete(editorial.getId());
+			break;
+		}
+		
+        return "redirect:/editoriales";
+	}
 }
