e284647ac29d77a99eff51d43252796d90bb1ada
Ana Martínez Delgado <martinezdelgadoana@gmail.com>
2018-11-15 14:13:33 +0100

    SpringREST

---
+++ springrest/src/main/java/com/ipartek/formacion/springrest/apirest/SaludoController.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.springrest.apirest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ipartek.formacion.springrest.modelos.Saludo;
+
+@RestController
+public class SaludoController {
+	private static final String plantilla = "Hola, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/saludo")
+    public Saludo saludar(@RequestParam(value="nombre", defaultValue="Mundo") String nombre) {
+        return new Saludo(counter.incrementAndGet(),
+                            String.format(plantilla, nombre));
+    }
+    
+    @GetMapping("/datos")
+    public List<Saludo> saludos() {
+    	List<Saludo> datos = new ArrayList<>();
+    	
+    	for(int i = 1; i <= 3; i++) {
+    		datos.add(new Saludo(i, "Hola" + i));
+    	}
+    	
+    	return datos;
+    }
+}
