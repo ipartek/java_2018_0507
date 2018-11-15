0348d9e713427f0c889d97bce240488b7d58eed5
JavierLete <JavierLete@10.0.2.15>
2018-11-15 14:10:34 +0100

    Cliente jQuery

--- springrest/src/main/resources/static/js/index.js
+++ springrest/src/main/resources/static/js/index.js
@@ -1,6 +1,24 @@
 $(function() {
-	$.getJSON('/saludo', function(respuesta) {
-		console.log(respuesta);
-		$('#bienvenida').text(respuesta.contenido);
-	})
+	$.getJSON('/saludo', respuestarest);
+	
+	$.getJSON('/datos', function(datos) {
+		$(datos).each(function() {
+			console.log(this);
+			//$('<tr><th>' + this.id + '</th><td>' + this.contenido + '</td></tr>').appendTo($('tbody'));
+			$('tbody').append('<tr><th>' + this.id + '</th><td>' + this.contenido + '</td></tr>');
+		});
+	});
+	
+	$('#fsaludo').submit(function(e) {
+		e.preventDefault();
+		
+		var nombre = $('#nombre').val();
+		
+		$.getJSON('/saludo?nombre=' + nombre, respuestarest)
+	});
 });
+
+function respuestarest(respuesta) {
+	console.log(respuesta);
+	$('#bienvenida').text(respuesta.contenido);
+}
