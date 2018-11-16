e6b51ea4b9a51a88b5250cef4f864cbaffd666de
JavierLete <JavierLete@10.0.2.15>
2018-11-16 14:31:29 +0100

    API REST y prueba de jQuery de obtener todas las editoriales

---
+++ prestamolibrosspring/src/main/resources/static/js/editoriales.js
@@ -0,0 +1,9 @@
+$(function() {
+	$.getJSON('/api/editoriales', function(editoriales) {
+		console.log(editoriales);
+		
+		$(editoriales).each(function() {
+			$('tbody').append('<tr><th>' + this.id + "</th><td>" + this.nombre + '</td></tr>');
+		});
+	});
+});
