0ef898bbe9f3d56fc768654e9c4fd5e435875e9a
Mikel Gil <m.gil05@gmail.com>
2018-11-22 17:54:50 +0100

    Remove some imports

--- editorialrestspring/src/main/java/com/ipartek/formacion/editorialrestspring/repositories/EditorialJDBCRepository.java
+++ editorialrestspring/src/main/java/com/ipartek/formacion/editorialrestspring/repositories/EditorialJDBCRepository.java
@@ -1,13 +1,11 @@
 package com.ipartek.formacion.editorialrestspring.repositories;
 
-import java.sql.Types;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
