3027353c4fad4e5150872db6c8fda3fb15649cc0
JavierLete <JavierLete@10.0.2.15>
2018-11-23 12:17:00 +0100

    Servicio REST de libros con repositorio TreeMap

---
+++ prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/repositories/LibroMapRepository.java
@@ -0,0 +1,66 @@
+package com.ipartek.formacion.prestamolibrosspring.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.springframework.stereotype.Repository;
+
+import com.ipartek.formacion.prestamolibrosspring.model.Editorial;
+import com.ipartek.formacion.prestamolibrosspring.model.Libro;
+
+@Repository
+public class LibroMapRepository implements CrudAble<Libro> {
+
+	private TreeMap<Long, Libro> libros = new TreeMap<>();
+	
+	public LibroMapRepository() {
+		insert(new Libro(1L, "Titulo1", "123456789", new Editorial(1L, "Editorial1")));
+		insert(new Libro(2L, "Titulo2", "223456789", new Editorial(2L, "Editorial2")));
+	}
+	
+	@Override
+	public List<Libro> getAll() {
+		return new ArrayList<Libro>(libros.values());
+	}
+
+	@Override
+	public Libro getById(Long id) {
+		return libros.get(id);
+	}
+
+	@Override
+	public Long insert(Libro libro) {
+		Long idAnterior;
+		
+		if(libros.size() == 0)
+			idAnterior = 0L;
+		else
+			idAnterior = libros.lastKey(); 
+		
+		libro.setId(idAnterior + 1);
+		
+		libros.put(libro.getId(), libro);
+		
+		return libro.getId();
+	}
+
+	@Override
+	public void update(Libro libro) {
+		if(!libros.containsKey(libro.getId())) {
+			throw new RepositoryException("El libro no existe, por lo que no se puede modificar"); 
+		}
+		
+		libros.put(libro.getId(), libro);
+	}
+
+	@Override
+	public void delete(Long id) {
+		if(!libros.containsKey(id)) {
+			throw new RepositoryException("El libro no existe, por lo que no se puede modificar"); 
+		}
+		
+		libros.remove(id);
+	}
+
+}
