dfc1a984dbccb94a0545d5f1f24d2b965b2af817
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-11-26 14:31:02 +0100

    proyecto master

---
+++ prestamolibrosspring/prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/repositories/EditorialMapRepository.java
@@ -0,0 +1,53 @@
+package com.ipartek.formacion.prestamolibrosspring.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import com.ipartek.formacion.prestamolibrosspring.model.Editorial;
+
+public class EditorialMapRepository implements CrudAble<Editorial>{
+	private TreeMap<Long, Editorial> editoriales = new TreeMap<>(); 
+	
+	public EditorialMapRepository() {
+		editoriales.put(1L, new Editorial(1L, "Anaya"));
+		editoriales.put(2L, new Editorial(2L, "McGraw Hill"));
+	}
+	
+	@Override
+	public List<Editorial> getAll() {
+		return new ArrayList<Editorial>(editoriales.values());
+	}
+
+	@Override
+	public Editorial getById(Long id) {
+		return editoriales.get(id);
+	}
+
+	@Override
+	public Long insert(Editorial e) {
+		Long idAnterior;
+		
+		if(editoriales.size() == 0)
+			idAnterior = 0L;
+		else
+			idAnterior = editoriales.lastKey(); 
+		
+		e.setId(idAnterior + 1);
+		
+		editoriales.put(e.getId(), e);
+		
+		return e.getId();
+	}
+
+	@Override
+	public void update(Editorial e) {
+		editoriales.put(e.getId(), e); //Si ya existe el id sustituye el objeto en lugar de crear uno nuevo
+	}
+
+	@Override
+	public void delete(Long id) {
+		editoriales.remove(id);
+	}
+
+}
---
+++ prestamolibrosspring/prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/repositories/EditorialMySqlJdbcTemplateRepository.java
@@ -0,0 +1,86 @@
+package com.ipartek.formacion.prestamolibrosspring.repositories;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.ipartek.formacion.prestamolibrosspring.model.Editorial;
+
+@Repository
+public class EditorialMySqlJdbcTemplateRepository implements CrudAble<Editorial> {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public List<Editorial> getAll() {
+		// Compatible con <Java8
+		return jdbcTemplate.query("call getAll()", new EditorialMapper());
+
+//		return jdbcTemplate.query( "select id, nombre from editoriales", new RowMapper<Editorial>() {
+//		    public Editorial mapRow(ResultSet rs, int rowNum) throws SQLException {
+//		    	return new Editorial(rs.getLong("id"), rs.getString("nombre"));
+//		    }
+//		});
+
+		// return jdbcTemplate.query("select id, nombre from editoriales", (rs, rowNum)
+		// -> // >=Java8
+		// new Editorial(rs.getLong("id"), rs.getString("nombre")));
+	}
+
+	@Override
+	public Editorial getById(Long id) {
+		return jdbcTemplate.queryForObject("call getById(?)", new Object[] { id },
+				new EditorialMapper());
+	}
+
+	@Override
+	public Long insert(Editorial editorial) {
+		return jdbcTemplate.queryForObject("call insertar(?)",
+				new Object[] { editorial.getNombre() }, Long.class);
+
+//		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+//		>=Java8
+//		jdbcTemplate.update(connection -> {
+//			PreparedStatement ps = connection.prepareStatement("insert into editoriales (nombre) values (?)",
+//				Statement.RETURN_GENERATED_KEYS);
+//			ps.setString(1, editorial.getNombre());
+//			return ps;
+//		}, keyHolder);
+
+		// <Java8
+//		jdbcTemplate.update(new PreparedStatementCreator() {
+//			@Override
+//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//				PreparedStatement ps = con.prepareStatement("insert into editoriales (nombre) values (?)", 
+//						Statement.RETURN_GENERATED_KEYS);
+//				ps.setString(1, editorial.getNombre());
+//				return ps;
+//			}
+//		}, keyHolder);
+		
+//		return (long) keyHolder.getKey();
+	}
+
+	@Override
+	public void update(Editorial editorial) {
+		jdbcTemplate.update("call modificar(?,?)",
+				new Object[] { editorial.getNombre(), editorial.getId() });
+	}
+
+	@Override
+	public void delete(Long id) {
+		jdbcTemplate.update("call borrar(?)", new Object[] { id });
+	}
+
+	private static final class EditorialMapper implements RowMapper<Editorial> {
+		public Editorial mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return new Editorial(rs.getLong("id"), rs.getString("nombre"));
+		}
+	}
+}
