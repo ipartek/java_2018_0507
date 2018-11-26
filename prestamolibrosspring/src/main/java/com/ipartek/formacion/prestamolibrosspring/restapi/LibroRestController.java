45fb5ed67b9c63063d76e9b52a62502aa99f37da
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-11-26 17:57:54 +0100

    Funcionando el RestAble con el procedure inner join

--- prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/restapi/LibroRestController.java
+++ prestamolibrosspring/src/main/java/com/ipartek/formacion/prestamolibrosspring/restapi/LibroRestController.java
@@ -3,49 +3,46 @@ package com.ipartek.formacion.prestamolibrosspring.restapi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.prestamolibrosspring.model.Libro;
-import com.ipartek.formacion.prestamolibrosspring.repositories.CrudAble;
+
 
 @RestController
 public class LibroRestController implements RestAble<Libro> {
 	@Autowired
-	private CrudAble<Libro> repositorio;
-	
-	@Override
+	private RestAble<Libro> repositorio;
+
 	@GetMapping("/api/libros")
 	public List<Libro> obtenerTodos() {
-		return repositorio.getAll();
+		return repositorio.obtenerTodos();
+	}
+
+
+	@Override
+	public Libro obtenerPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	@GetMapping("/api/libros/{id}")
-	public Libro obtenerPorId(@PathVariable("id") Long id) {
-		return repositorio.getById(id);
+	public Long crear(Libro t) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	@PostMapping("/api/libros")
-	public Long crear(@RequestBody Libro libro) {
-		return repositorio.insert(libro);
+	public void modificar(Libro t) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	@PutMapping("/api/libros")
-	public void modificar(@RequestBody Libro libro) {
-		repositorio.update(libro);
+	public void borrar(Long id) {
+		// TODO Auto-generated method stub
+		
 	}
+	
 
-	@Override
-	@DeleteMapping("/api/libros/{id}")
-	public void borrar(@PathVariable("id") Long id) {
-		repositorio.delete(id);
-	}
 }
