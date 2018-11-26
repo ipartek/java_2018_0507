c3a1546a43373b19a972dcdd8ecb83d656cac44b
Ana Martínez Delgado <martinezdelgadoana@gmail.com>
2018-11-26 15:20:29 +0100

    sql modif para que funcione

---
+++ prestamolibrosspring_javi/sql/prestamolibros.sql
@@ -0,0 +1,151 @@
+-- MySQL dump 10.16  Distrib 10.1.36-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: prestamolibros
+-- ------------------------------------------------------
+-- Server version	10.1.36-MariaDB
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,ANSI' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: "prestamolibros"
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ "prestamolibros" /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE "prestamolibros";
+
+--
+-- Table structure for table "editoriales"
+--
+
+DROP TABLE IF EXISTS "editoriales";
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE "editoriales" (
+  "id" int(11) NOT NULL AUTO_INCREMENT,
+  "nombre" varchar(45) DEFAULT NULL,
+  PRIMARY KEY ("id")
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table "editoriales"
+--
+
+LOCK TABLES "editoriales" WRITE;
+/*!40000 ALTER TABLE "editoriales" DISABLE KEYS */;
+INSERT INTO "editoriales" VALUES (1,'Javier Lete & asociados'),(4,'Wrox'),(5,'JavierLete'),(9,'PepePerez'),(10,'Yepa'),(13,'Nuevo');
+/*!40000 ALTER TABLE "editoriales" ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'prestamolibros'
+--
+/*!50003 DROP PROCEDURE IF EXISTS "borrar" */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `borrar`(IN rid int(11))
+BEGIN
+	delete from editoriales where id = rid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS "getAll" */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAll`()
+BEGIN
+	select id, nombre from editoriales;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS "getById" */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getById`(IN rid int(11))
+BEGIN
+	select id, nombre from editoriales where id = rid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS "insertar" */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar`(IN rnombre VARCHAR(45))
+BEGIN
+	insert into editoriales (nombre) values (rnombre);
+    select LAST_INSERT_ID();
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS "modificar" */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `modificar`(IN rnombre VARCHAR(45), IN rid int(11))
+BEGIN
+	update editoriales set nombre = rnombre where id = rid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-26 15:07:12
