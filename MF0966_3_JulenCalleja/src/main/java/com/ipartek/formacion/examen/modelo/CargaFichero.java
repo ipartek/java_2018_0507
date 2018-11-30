7104db8d921e49d815c4d3d9c0769fc2b172a474
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-11-30 09:25:00 +0100

    adsfads

---
+++ MF0966_3_JulenCalleja/src/main/java/com/ipartek/formacion/examen/modelo/CargaFichero.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.examen.modelo;
+
+class CargaFichero {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ MF0966_3_JulenCalleja/src/main/java/com/ipartek/formacion/examen/modelo/Gente.java
@@ -0,0 +1,143 @@
+package com.ipartek.formacion.examen.modelo;
+
+public class Gente {
+
+	private long id;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private int nPortal;
+	private String email;
+	private String dni;
+	private String puesto;
+	
+	
+	public Gente(long id, String nombre, String apellido, String direccion, int nPortal, String email, String dni,
+			String puesto) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+		this.nPortal = nPortal;
+		this.email = email;
+		this.dni = dni;
+		this.puesto = puesto;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getnPortal() {
+		return nPortal;
+	}
+	public void setnPortal(int nPortal) {
+		this.nPortal = nPortal;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + nPortal;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((puesto == null) ? 0 : puesto.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gente other = (Gente) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nPortal != other.nPortal)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (puesto == null) {
+			if (other.puesto != null)
+				return false;
+		} else if (!puesto.equals(other.puesto))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Gente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion
+				+ ", nPortal=" + nPortal + ", email=" + email + ", dni=" + dni + ", puesto=" + puesto + "]";
+	}
+	
+	
+	
+}
