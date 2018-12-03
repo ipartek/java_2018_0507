924541311ee7c90f7b185e6dd521d68ea72863dd
Javier Lete <jglete@eskura21.com>
2018-12-03 17:50:16 +0100

    Proyecto SpringBoot Web y Dev Tools

---
+++ testjsp/src/main/java/com/javierlete/testjsp/TestjspApplication.java
@@ -0,0 +1,12 @@
+package com.javierlete.testjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjspApplication.class, args);
+	}
+}
