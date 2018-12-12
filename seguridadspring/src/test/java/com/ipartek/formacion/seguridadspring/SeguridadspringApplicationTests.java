587eca3df99c527863a197dce7dc48962f2283b0
Javier Lete <jglete@eskura21.com>
2018-12-12 17:45:59 +0100

    Seguridad / Spring Starter Project (Web, Security, Thymeleaf, DevTools)

---
+++ seguridadspring/src/test/java/com/ipartek/formacion/seguridadspring/SeguridadspringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ipartek.formacion.seguridadspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeguridadspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
