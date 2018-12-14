c362844b84b7687d2f68d3536a1aa6fd687bb84d
Curso <Curso@PORT-Z7.ipartekaula.com>
2018-12-14 09:06:15 +0100

    borrado de import

--- SpringFormularios/src/main/java/com/ipartek/spring/controller/HelloController.java
+++ SpringFormularios/src/main/java/com/ipartek/spring/controller/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
